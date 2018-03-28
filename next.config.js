6313f6c123db48e3590ffb2cf7eec710639515dd
Steve Astels <sastels@gmail.com>
2018-03-28 11:33:29 -0400

    lint in dev compile

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  webpack: (config) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
+    webpack: (config, { dev }) => {
+        if (dev) {
+            config.module.rules.push({
+                test: /\.js$/,
+                exclude: [/node_modules/, /lib/],
+                loader: 'eslint-loader',
+                options: {
+                    // Emit errors as warnings for dev to not break webpack build.
+                    // Eslint errors are shown in console for dev, yay :-)
+                    emitWarning: dev,
+                }
+            })
+        }
+        config.node = {
+            // Fixes npm packages that depend on `fs` module
+            fs: 'empty'
+        }
+        return config
     }
-
-    return config
-  }
 }
