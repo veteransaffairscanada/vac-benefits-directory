49eaa7331df1895e16e7070b3eb0acad7aecc505
Steve Astels <sastels@gmail.com>
2018-04-03 09:38:16 -0400

    fix test

--- __tests__/index_test.js
+++ __tests__/index_test.js
@@ -1,21 +1,19 @@
 /* eslint-env jest */
 
-import { mount } from 'enzyme'
-import React from 'react'
+import { mount } from "enzyme";
+import React from "react";
 
-import App from '../pages/index.js'
+import App from "../pages/index.js";
 
-
-jest.mock('react-i18next', () => ({
-    // this mock makes sure any components using the translate HoC receive the t function as a prop
-    translate: () => Component => props => <Component t={(key) => key} {...props} />, // eslint-disable-line react/display-name
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  translate: () => Component => props => <Component t={key => key} {...props} /> // eslint-disable-line react/display-name
 }));
 
+describe("With Enzyme", () => {
+  it("index page", () => {
+    const app = mount(<App />);
 
-describe('With Enzyme', () => {
-    it('index page', () => {
-        const app = mount(<App />)
-
-        expect(app.find('p').text()).toEqual('welcome')
-    })
-})
+    expect(app.find("p").text()).toEqual("poc-description");
+  });
+});
