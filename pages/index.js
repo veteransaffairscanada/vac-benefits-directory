37d04f541724b67a7af0b1a93fd3fe9c5dfc9d28
Steve Astels <sastels@gmail.com>
2018-04-03 13:10:14 -0400

    add a button to change language

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { withI18next } from "../lib/withI18next";
-import { GoCSignature } from "@cdssnc/gcui";
+import { GoCSignature, Button } from "@cdssnc/gcui";
 import styles from "../styles/styles.scss";
 
-export default withI18next(["home"])((
-  { t, initialI18nStore } // eslint-disable-line no-unused-vars
-) => (
-  <div>
-    <GoCSignature width="20em" />
-    <p className={styles.example}>{t("poc-description")}</p>
-  </div>
-));
+class App extends Component {
+  render() {
+    const { i18n, t } = this.props; // eslint-disable-line no-unused-vars
+
+    return (
+      <div>
+        <GoCSignature width="20em" />
+        <p className={styles.example}>{t("poc-description")}</p>
+        <Button
+          className={styles.button}
+          onClick={() =>
+            i18n.changeLanguage(
+              i18n.language.substring(0, 2) === "en" ? "fr" : "en"
+            )
+          }
+        >
+          {i18n.language.substring(0, 2) === "en" ? "Français" : "English"}
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default withI18next(["home"])(App);
