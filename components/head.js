8e65143352aad1f31c7324eb69cf650866be0220
Max Neuvians <max@neuvians.net>
2018-04-04 11:04:18 -0400

    Bootstrap 4 integration for POC purposes

--- components/head.js
+++ components/head.js
@@ -39,6 +39,10 @@ const Head = (props: Props) => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    />
   </NextHead>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,19 @@ import "core-js/es6/set";
 import "raf/polyfill";
 
 import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem
+} from "reactstrap";
+
 import { withI18next } from "../lib/withI18next";
 import { GoCSignature, Button } from "@cdssnc/gcui";
+import Head from "../components/head";
 import styles from "../styles/styles.scss";
 
 type Props = {
@@ -23,14 +34,29 @@ class App extends Component<Props> {
 
     return (
       <div>
-        <GoCSignature width="20em" />
-        <p className={styles.example}>{t("poc-description")}</p>
-        <Button
-          className={styles.button}
-          onClick={() => i18n.changeLanguage(t("other-language-code"))}
-        >
-          {t("other-language")}
-        </Button>
+        <Head />
+        <Navbar color="light" light>
+          <NavbarBrand href="/">
+            <GoCSignature width="20em" />
+          </NavbarBrand>
+          <Nav>
+            <NavItem>
+              <Button
+                className={styles.button}
+                onClick={() => i18n.changeLanguage(t("other-language-code"))}
+              >
+                {t("other-language")}
+              </Button>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col xs="12">
+              <p className={styles.example}>{t("poc-description")}</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
