ece50df249cf207173aee089c8d74198256bd108
Steve Astels <sastels@users.noreply.github.com>
2018-04-13 13:27:50 -0400

    Page A2 and maybe fix deploy (#68)

--- __tests__/A2_test.js
+++ __tests__/A2_test.js
@@ -3,7 +3,7 @@
 import { mount } from "enzyme";
 import React from "react";
 
-import App from "../pages/A1";
+import App from "../pages/A2";
 
 jest.mock("react-ga");
 jest.mock("react-i18next", () => ({
@@ -11,11 +11,10 @@ jest.mock("react-i18next", () => ({
   translate: () => Component => props => <Component t={key => key} {...props} /> // eslint-disable-line react/display-name
 }));
 
-describe("Page A1", () => {
+describe("Page A2", () => {
   it("Instructions", () => {
     const app = mount(<App i18n={{ language: "en-US" }} />);
-    expect(app.text()).toMatch(/A1.What services are you interested in?/);
-    expect(app.text()).toMatch(/A1.Select all that apply/);
+    expect(app.text()).toMatch(/A2.What best describes your status?/);
   });
 
   it("Buttons", () => {
@@ -23,16 +22,13 @@ describe("Page A1", () => {
     const buttons = app.find("Button");
     const expectedText = [
       "other-language",
-      "A1.Financial Support",
-      "A1.Rehabilitation",
-      "A1.Mental Health Services",
-      "A1.Health Care",
-      "A1.Career Transition",
-      "A1.Support for Families",
-      "A1.Next",
+      "A2.Veteran",
+      "A2.Family",
+      "A2.Not Sure",
+      "A2.See Results",
       "Privacy"
     ];
-    expect(buttons.length).toEqual(9);
+    expect(buttons.length).toEqual(6);
     buttons.map(function(button, index) {
       expect(button.text()).toEqual(expectedText[index]);
     });
