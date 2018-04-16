891072145f8f69a139762b804f8aa57b6f9d2630
Steve Astels <sastels@gmail.com>
2018-04-16 12:59:01 -0400

    add test

---
+++ __tests__/select_button_test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SelectButton from "../components/select_button";
+import { mount } from "enzyme";
+
+describe("Test SelectButton", () => {
+  it("SelectButton renders", () => {
+    const button = mount(
+      <SelectButton
+        t={key => key}
+        id="test_id"
+        text="test_text"
+        onClick={() => {}}
+        isDown={false}
+      />
+    );
+    expect(button.text()).toEqual("test_text");
+  });
+});
