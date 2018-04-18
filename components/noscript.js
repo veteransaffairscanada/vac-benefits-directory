5f3158c457467335fe81ea03c9c4f66940620f5f
Max Neuvians <max@neuvians.net>
2018-04-18 14:45:08 -0400

    Added a modal that will show up in the noscript tag with a hard link to the all-benefits page

---
+++ components/noscript.js
@@ -0,0 +1,67 @@
+// @flow
+
+import React, { Component } from "react";
+
+import styled from "react-emotion";
+
+type Props = {
+  t: mixed
+};
+
+const Modal = styled("div")`
+  left: 50%;
+  margin: -250px 0 0 -32%;
+  opacity: 1;
+  position: absolute;
+  top: 50%;
+  visibility: visible;
+  width: 65%;
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
+  transition: all 0.4s ease-in-out;
+  -moz-transition: all 0.4s ease-in-out;
+  -webkit-transition: all 0.4s ease-in-out;
+`;
+
+const ModalContent = styled("div")`
+  background: #ff6961;
+  position: relative;
+  z-index: 20;
+  border-radius: 5px;
+  color: #fff;
+  padding: 30px;
+`;
+
+const Overlay = styled("div")`
+  background-color: #000;
+  background: rgba(0, 0, 0, 0.8);
+  height: 100%;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 10;
+`;
+
+class Noscript extends Component<Props> {
+  props: Props;
+
+  render() {
+    const { t } = this.props;
+
+    return (
+      <noscript>
+        <Modal>
+          <ModalContent>
+            <div className="copy">
+              <p dangerouslySetInnerHTML={{ __html: t("noscript") }} />
+            </div>
+          </ModalContent>
+          <Overlay />
+        </Modal>
+      </noscript>
+    );
+  }
+}
+
+export default Noscript;
