5942bf0d4bf6df4e25d126713430a3c2cf4f2736
Steve Astels <sastels@gmail.com>
2018-04-20 15:19:29 -0400

    get A3 tests working

--- __tests__/pages/A3_test.js
+++ __tests__/pages/A3_test.js
@@ -7,8 +7,8 @@ import { App } from "../../pages/A3";
 
 const tMocked = key => key;
 const i18nFixture = { language: "en-US" };
-const selectedFixture = ["Financial Support", "Rehabilitation"];
-const userFixture = ["Veteran", "Family"];
+const selectedFixture = ["rec3PfnqeqyxSbx1x", "recQO4AHswOl75poF"];
+const userFixture = ["rec726lY5vUBEh2Sv", "recDAuNt8DXhD88Mr"];
 const urlFixture = {
   query: {
     selected: selectedFixture.join(),
@@ -16,16 +16,46 @@ const urlFixture = {
   }
 };
 
-const benefitListFixture = [
+const benefitTypesFixture = [
   {
-    type: "Support for Families",
-    title: "Survivor's Pension",
-    description: "Survivor's Pension Description"
+    id: "rec3PfnqeqyxSbx1x",
+    name_en: "Compensation For Harm",
+    name_fr: "Compensation Pour Préjudice"
   },
   {
-    type: "Financial Support",
-    title: "Disability Award",
-    description: "Disability Award Description"
+    id: "recQO4AHswOl75poF",
+    name_en: "Healthcare Cost Coverage",
+    name_fr: "Couverture des Coûts de Soins de Santé"
+  }
+];
+
+const patronTypesFixture = [
+  {
+    id: "rec726lY5vUBEh2Sv",
+    name_en: "Military Service-Person",
+    name_fr: "Service militaire-Personne"
+  },
+  {
+    id: "recDAuNt8DXhD88Mr",
+    name_en: "RCMP Service-Person",
+    name_fr: "Personne-Service de la GRC"
+  }
+];
+
+const benefitsFixture = [
+  {
+    patron_types: ["rec726lY5vUBEh2Sv"],
+    benefit_types: ["rec3PfnqeqyxSbx1x"],
+    id: "recQtMLSsyS90o4rV",
+    vac_name_en: "Disability Award",
+    vac_name_fr: "Prix ​​d'invalidité"
+  },
+  {
+    patron_types: ["rec726lY5vUBEh2Sv"],
+    benefit_types: ["rec3PfnqeqyxSbx1x"],
+    id: "recvzRaT9ormprNkb",
+    vac_name_en: "Disability Pension",
+    vac_name_fr: "Pension d'invalidité"
   }
 ];
 
@@ -34,7 +64,15 @@ jest.mock("react-ga");
 describe("Page A3", () => {
   it("Benefit count string no benefits", () => {
     const app = mount(
-      <App i18n={i18nFixture} t={tMocked} benefitList={[]} url={urlFixture} />
+      <App
+        i18n={i18nFixture}
+        t={tMocked}
+        storeHydrated={true}
+        benefitTypes={[]}
+        patronTypes={[]}
+        benefits={[]}
+        url={urlFixture}
+      />
     );
     expect(app.find("p#benefitCountString").text()).toEqual(
       "A3.Based on your selections you do not qualify for any benefits at this time"
@@ -46,7 +84,10 @@ describe("Page A3", () => {
       <App
         i18n={i18nFixture}
         t={tMocked}
-        benefitList={[benefitListFixture[0]]}
+        storeHydrated={true}
+        benefitTypes={benefitTypesFixture}
+        patronTypes={patronTypesFixture}
+        benefits={[benefitsFixture[0]]}
         url={urlFixture}
       />
     );
@@ -60,7 +101,10 @@ describe("Page A3", () => {
       <App
         i18n={i18nFixture}
         t={tMocked}
-        benefitList={benefitListFixture}
+        storeHydrated={true}
+        benefitTypes={benefitTypesFixture}
+        patronTypes={patronTypesFixture}
+        benefits={benefitsFixture}
         url={urlFixture}
       />
     );
@@ -71,11 +115,19 @@ describe("Page A3", () => {
 
   it("Selected Options", () => {
     const app = mount(
-      <App i18n={i18nFixture} t={tMocked} benefitList={[]} url={urlFixture} />
+      <App
+        i18n={i18nFixture}
+        t={tMocked}
+        storeHydrated={true}
+        benefitTypes={benefitTypesFixture}
+        patronTypes={patronTypesFixture}
+        benefits={[]}
+        url={urlFixture}
+      />
     );
-    const expectedText = selectedFixture
-      .map(s => {
-        return "A1." + s;
+    const expectedText = benefitTypesFixture
+      .map(bt => {
+        return bt.name_fr;
       })
       .join("");
     expect(app.find("SelectedOptionsCard#vacServicesCard").text()).toEqual(
@@ -85,11 +137,19 @@ describe("Page A3", () => {
 
   it("Selected user types", () => {
     const app = mount(
-      <App i18n={i18nFixture} t={tMocked} benefitList={[]} url={urlFixture} />
+      <App
+        i18n={i18nFixture}
+        t={tMocked}
+        storeHydrated={true}
+        benefitTypes={benefitTypesFixture}
+        patronTypes={patronTypesFixture}
+        benefits={[]}
+        url={urlFixture}
+      />
     );
-    const expectedText = userFixture
-      .map(s => {
-        return "A2." + s;
+    const expectedText = patronTypesFixture
+      .map(pt => {
+        return pt.name_fr;
       })
       .join("");
     expect(app.find("SelectedOptionsCard#userStatusesCard").text()).toEqual(
