889f945d5ecdd835c1a0cf010fd0ad23e210b7aa
Steve Astels <sastels@gmail.com>
2018-04-30 16:36:22 -0400

    refactor slightly

--- cypress/integration/A_mock_airtable_spec.js
+++ cypress/integration/A_mock_airtable_spec.js
@@ -17,79 +17,77 @@ const benefitTypesFixture = {
   ]
 };
 
-describe("Spy / Stub", () => {
-  describe("Spying", () => {
-    beforeEach(() => {
-      cy.visit(baseURL, {
-        onBeforeLoad(win) {
-          cy.spy(win, "fetch");
-        }
-      });
-    });
-
-    it("Requests Benefit Types from Airtable", () => {
-      cy.contains("What services are you interested in?");
-      cy
-        .window()
-        .its("fetch")
-        .should("be.calledWith", benefitTypesURL);
+describe("Spy on Airtable fetch", () => {
+  beforeEach(() => {
+    cy.visit(baseURL, {
+      onBeforeLoad(win) {
+        cy.spy(win, "fetch");
+      }
     });
   });
 
-  describe("Stubbing", () => {
-    let fetchBTDeferred;
-
-    beforeEach(() => {
-      fetchBTDeferred = deferred();
-      cy.visit(baseURL, {
-        onBeforeLoad(win) {
-          cy
-            .stub(win, "fetch")
-            .withArgs(benefitTypesURL)
-            .as("fetchBT")
-            .returns(fetchBTDeferred.promise);
-        }
-      });
-    });
-
-    it("Requests Benefit Types from Airtable", () => {
-      cy.get("@fetchBT").should("be.calledWith", benefitTypesURL);
-    });
-
-    describe("When Airtable returned a Benefit Type", function() {
-      beforeEach(function() {
-        fetchBTDeferred.resolve({
-          json: () => {
-            return benefitTypesFixture;
-          },
-          ok: true
-        });
-      });
-
-      it("Has a Benefit Type button", function() {
-        cy.get("#rec3PfnqeqyxSbx1x");
-      });
-
-      it("Can add Benefit Type to url", function() {
-        cy.get("#rec3PfnqeqyxSbx1x").click();
-        cy.contains("Next").click();
-        cy
-          .url()
-          .should(
-            "equal",
-            baseURL +
-              "?section=A2&selectedBenefitTypes=rec3PfnqeqyxSbx1x&selectedPatronTypes="
-          );
-      });
-    });
+  it("Requests Benefit Types from Airtable", () => {
+    cy.contains("What services are you interested in?");
+    cy
+      .window()
+      .its("fetch")
+      .should("be.calledWith", benefitTypesURL);
   });
-
-  function deferred() {
-    const deferred = {};
-    deferred.promise = new Promise((resolve, reject) => {
-      deferred.resolve = resolve;
-      deferred.reject = reject;
-    });
-    return deferred;
-  }
 });
+
+describe("Stub Airtable fetch", () => {
+  let fetchBTDeferred;
+
+  beforeEach(() => {
+    fetchBTDeferred = deferred();
+    cy.visit(baseURL, {
+      onBeforeLoad(win) {
+        cy
+          .stub(win, "fetch")
+          .withArgs(benefitTypesURL)
+          .as("fetchBT")
+          .returns(fetchBTDeferred.promise);
+      }
+    });
+  });
+
+  it("Requests Benefit Types from Airtable", () => {
+    cy.get("@fetchBT").should("be.calledWith", benefitTypesURL);
+  });
+
+  describe("When Airtable returned a Benefit Type", function() {
+    beforeEach(function() {
+      fetchBTDeferred.resolve({
+        json: () => {
+          return benefitTypesFixture;
+        },
+        ok: true
+      });
+    });
+
+    it("Has a Benefit Type button", function() {
+      cy.get("#rec3PfnqeqyxSbx1x");
+    });
+
+    it("Can add Benefit Type to url", function() {
+      cy.get("#rec3PfnqeqyxSbx1x").click();
+      cy.contains("Next").click();
+      cy
+        .url()
+        .should(
+          "equal",
+          baseURL +
+            "?section=A2&selectedBenefitTypes=rec3PfnqeqyxSbx1x&selectedPatronTypes="
+        );
+    });
+  });
+});
+
+function deferred() {
+  const deferred = {};
+  deferred.promise = new Promise((resolve, reject) => {
+    deferred.resolve = resolve;
+    deferred.reject = reject;
+  });
+  return deferred;
+}
