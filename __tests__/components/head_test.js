455bc9eeba4c99d1f17a98ebd3f205067038381d
Steve Astels <sastels@gmail.com>
2018-05-01 14:56:29 -0400

    Head test - needs more work

---
+++ __tests__/components/head_test.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Head from "../../components/head";
+import { mount } from "enzyme";
+
+describe("Head", () => {
+  // Setup
+
+  let props;
+  let _mountedHead;
+  const mountedHead = () => {
+    if (!_mountedHead) {
+      _mountedHead = mount(<Head {...props} />);
+    }
+    return _mountedHead;
+  };
+
+  beforeEach(() => {
+    window.GA_INITIALIZED = true;
+
+    props = {
+      t: key => key
+    };
+    _mountedHead = undefined;
+  });
+
+  // Tests
+
+  // TODO test with a page maybe? https://github.com/zeit/next.js/issues/556
+  /*
+  it("has default title", () => {
+
+    console.log(mountedHead().html())
+
+    expect(
+      mountedHead()
+        .find("#title")
+        .at(0)
+        .text()
+    ).toEqual("title");
+  })
+
+  it("has default description", () => {
+    expect(mountedHead()
+      .find('[name="description"]')
+      .at(0)
+      .text()
+    ).toEqual("description");
+  });
+
+  describe("with title and description set", () => {
+    beforeEach(() => {
+      props.title = "test title"
+      props.description = "test description"
+    })
+
+    it("has correct title and description", () => {
+      expect(
+        mountedHead()
+          .find("#title")
+          .at(0)
+          .text()
+      ).toEqual("test title");
+
+      expect(mountedHead()
+        .find('[name="description"]')
+        .at(0)
+        .text()
+      ).toEqual("test description");
+    })
+  });
+*/
+  it("logPageView called", () => {
+    let analytics = require("../../utils/analytics");
+    analytics.logPageView = jest.fn();
+    mountedHead();
+    expect(analytics.logPageView).toBeCalled();
+  });
+});
--- components/head.js
+++ components/head.js
@@ -7,14 +7,8 @@ import { initGA, logPageView } from "../utils/analytics";
 type Props = {
   title?: string,
   description?: string,
-  url?: string,
-  ogImage?: string,
   t: mixed
 };
-
-const defaultDescription = "";
-const defaultOGURL = "";
-const defaultOGImage = "";
 
 class Head extends Component<Props> {
   props: Props;
@@ -34,31 +28,10 @@ class Head extends Component<Props> {
         <title>{this.props.title || this.props.t("title")}</title>
         <meta
           name="description"
-          content={this.props.description || defaultDescription}
+          content={this.props.description || this.props.t("description")}
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-        <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-        <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
         <link rel="icon" href="/static/favicon.ico" />
-        <meta property="og:url" content={this.props.url || defaultOGURL} />
-        <meta property="og:title" content={this.props.title || ""} />
-        <meta
-          property="og:description"
-          content={this.props.description || defaultDescription}
-        />
-        <meta name="twitter:site" content={this.props.url || defaultOGURL} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image"
-          content={this.props.ogImage || defaultOGImage}
-        />
-        <meta
-          property="og:image"
-          content={this.props.ogImage || defaultOGImage}
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
