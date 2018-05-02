9c84fce43ad5b772fa69942b2df6f26cc5d52a06
Steve Astels <sastels@gmail.com>
2018-05-02 11:46:39 -0400

    BenefitTitleCard tests

--- __tests__/components/benefit_card_test.js
+++ __tests__/components/benefit_card_test.js
@@ -3,26 +3,67 @@ import { BenefitTitleCard, BenefitCard } from "../../components/benefit_cards";
 import { mount } from "enzyme";
 import { joinedBenefitsFixture } from "../fixtures/benefits";
 
-describe("Test Benefit Cards", () => {
-  it("BenefitTitleCard", () => {
-    const card = mount(
-      <BenefitTitleCard t={key => key} benefit={joinedBenefitsFixture[0]} />
-    );
-    expect(card.text()).toEqual(joinedBenefitsFixture[0].vac_name_fr);
+describe("BenefitTitleCard", () => {
+  // Setup
+
+  let props;
+  let _mountedBenefitTitleCard;
+  const mountedBenefitTitleCard = () => {
+    if (!_mountedBenefitTitleCard) {
+      _mountedBenefitTitleCard = mount(<BenefitTitleCard {...props} />);
+    }
+    return _mountedBenefitTitleCard;
+  };
+
+  beforeEach(() => {
+    props = {
+      t: () => "en",
+      benefit: joinedBenefitsFixture[0]
+    };
+    _mountedBenefitTitleCard = undefined;
   });
 
-  it("BenefitCard", () => {
-    const test_props = joinedBenefitsFixture[0];
-    const card = mount(<BenefitCard t={key => key} benefit={test_props} />);
-    expect(card.find("CardHeader").text()).toEqual(
-      test_props.benefitTypeFr.toUpperCase()
-    );
-    expect(card.find("Typography#title").text()).toEqual(
-      test_props.vac_name_fr
-    );
-    expect(card.find("Typography#description").text()).toEqual(
-      test_props.descriptionFr
-    );
-    expect(card.find("Button").text()).toEqual("View Details");
+  it("contains a SelectButton", () => {
+    expect(mountedBenefitTitleCard().find("SelectButton").length).toEqual(1);
+  });
+
+  it("has a blank target", () => {
+    expect(
+      mountedBenefitTitleCard()
+        .find("SelectButton")
+        .prop("target")
+    ).toEqual("_blank");
+  });
+
+  it("has expected English text and href", () => {
+    expect(
+      mountedBenefitTitleCard()
+        .find("SelectButton")
+        .text()
+    ).toEqual(joinedBenefitsFixture[0].vac_name_en);
+    expect(
+      mountedBenefitTitleCard()
+        .find("SelectButton")
+        .prop("href")
+    ).toEqual(joinedBenefitsFixture[0].linkEn);
+  });
+
+  describe("when language is French", () => {
+    beforeEach(() => {
+      props.t = () => "fr";
+    });
+
+    it("has expected French text and href", () => {
+      expect(
+        mountedBenefitTitleCard()
+          .find("SelectButton")
+          .text()
+      ).toEqual(joinedBenefitsFixture[0].vac_name_fr);
+      expect(
+        mountedBenefitTitleCard()
+          .find("SelectButton")
+          .prop("href")
+      ).toEqual(joinedBenefitsFixture[0].linkFr);
+    });
   });
 });
