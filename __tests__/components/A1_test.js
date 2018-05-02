849519cfe48558ec720b1c1a2c06af3b76fb7bd1
Steve Astels <sastels@gmail.com>
2018-05-02 13:25:17 -0400

    refactor A1 a bit

--- __tests__/components/A1_test.js
+++ __tests__/components/A1_test.js
@@ -7,18 +7,88 @@ import { App } from "../../components/A1";
 
 jest.mock("react-ga");
 
-const benefitTypesFixture = [
-  {
-    id: "rec3PfnqeqyxSbx1x",
-    name_en: "Compensation For Harm",
-    name_fr: "Compensation Pour Préjudice"
-  },
-  {
-    id: "recQO4AHswOl75poF",
-    name_en: "Healthcare Cost Coverage",
-    name_fr: "Couverture des Coûts de Soins de Santé"
-  }
-];
+import benefitTypesFixture from "../fixtures/benefit_types";
+/*
+
+- mounts with preselected components
+- toggleButton
+  - given id in state.selectedBenefitTypes, remove it from state
+  - given id not in state, add it to state
+
+
+
+
+
+
+import React from "react";
+import A1 from "../../components/A1";
+import { mount } from "enzyme";
+
+describe("A1", () => {
+  // Setup
+
+  let props;
+  let _mountedA1;
+  const mountedA1 = () => {
+    if (!_mountedA1) {
+      _mountedA1 = mount(<A1 {...props} />);
+    }
+    return _mountedA1;
+  };
+
+  beforeEach(() => {
+    props = {
+      t: key => key
+    };
+    _mountedA1 = undefined;
+  });
+
+  // Tests
+
+  it("shows the privacy button", () => {
+    expect(
+      mountedA1()
+        .find("#privacy")
+        .at(0)
+        .text()
+    ).toEqual("Privacy");
+  });
+
+  describe("in production environment", () => {
+    let _CIRCLE_SHA1;
+
+    beforeEach(() => {
+      _CIRCLE_SHA1 = process.env.CIRCLE_SHA1;
+      process.env.CIRCLE_SHA1 = "1234567890";
+    });
+
+    afterEach(() => {
+      if (typeof _CIRCLE_SHA1 === "undefined") {
+        delete process.env.CIRCLE_SHA1;
+      } else {
+        process.env.CIRCLE_SHA1 = _CIRCLE_SHA1;
+      }
+    });
+
+    it("shows abbreviated hash", () => {
+      expect(mountedA1().text()).toContain("1234567");
+    });
+  });
+
+  describe("defaults to node environment", () => {
+    beforeEach(() => {
+      delete process.env.CIRCLE_SHA1;
+    });
+
+    it("shows 'test' in test environment", () => {
+      expect(mountedA1().text()).toContain("test");
+    });
+  });
+});
+
+
+
+*/
 
 describe("Page A1", () => {
   it("Instructions", () => {
@@ -28,6 +98,7 @@ describe("Page A1", () => {
         t={key => key}
         storeHydrated={true}
         benefitTypes={[]}
+        selectedBenefitTypes={[]}
       />
     );
     expect(app.text()).toMatch(/A1.What services are you interested in?/);
@@ -64,6 +135,7 @@ describe("Page A1", () => {
         t={key => key}
         storeHydrated={true}
         benefitTypes={[]}
+        selectedBenefitTypes={[]}
       />
     );
     expect(app.find(".AllBenefits").text()).toEqual("Show All Benefits");
