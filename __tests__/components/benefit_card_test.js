bf82d14a971c917807308b73c5db6a696a57dc56
Steve Astels <sastels@gmail.com>
2018-05-02 11:29:19 -0400

    reorg fixtures

--- __tests__/components/benefit_card_test.js
+++ __tests__/components/benefit_card_test.js
@@ -1,40 +1,18 @@
 import React from "react";
 import { BenefitTitleCard, BenefitCard } from "../../components/benefit_cards";
 import { mount } from "enzyme";
-
-const benefitsFixture = [
-  {
-    vac_name_en: "Disability Award",
-    vac_name_fr: "Prix ​​d'invalidité",
-    benefitTypeEn: "BT EN 1",
-    benefitTypeFr: "BT FR 1",
-    descriptionEn: "Money compensation for a service related injury",
-    descriptionFr: "Compensation monétaire pour une blessure liée au service",
-    linkEn: "English link",
-    linkFr: "French link"
-  },
-  {
-    vac_name_en: "Disability Pension",
-    vac_name_fr: "Pension d'invalidité",
-    benefitTypeEn: "BT EN 2",
-    benefitTypeFr: "BT FR 2",
-    descriptionEn: "A pension compensating service related injuries",
-    descriptionFr: "Une blessure compensant les blessures liées au service",
-    linkEn: "English link",
-    linkFr: "French link"
-  }
-];
+import { joinedBenefitsFixture } from "../fixtures/benefits";
 
 describe("Test Benefit Cards", () => {
   it("BenefitTitleCard", () => {
     const card = mount(
-      <BenefitTitleCard t={key => key} benefit={benefitsFixture[0]} />
+      <BenefitTitleCard t={key => key} benefit={joinedBenefitsFixture[0]} />
     );
-    expect(card.text()).toEqual(benefitsFixture[0].vac_name_fr);
+    expect(card.text()).toEqual(joinedBenefitsFixture[0].vac_name_fr);
   });
 
   it("BenefitCard", () => {
-    const test_props = benefitsFixture[0];
+    const test_props = joinedBenefitsFixture[0];
     const card = mount(<BenefitCard t={key => key} benefit={test_props} />);
     expect(card.find("CardHeader").text()).toEqual(
       test_props.benefitTypeFr.toUpperCase()
---
+++ __tests__/fixtures/benefits.js
@@ -0,0 +1,39 @@
+export const benefitsFixture = [
+  {
+    patron_types: ["rec726lY5vUBEh2Sv"],
+    benefit_types: ["rec3PfnqeqyxSbx1x"],
+    id: "recQtMLSsyS90o4rV",
+    vac_name_en: "Disability Award",
+    vac_name_fr: "Prix ​​d'invalidité"
+  },
+  {
+    patron_types: ["rec726lY5vUBEh2Sv"],
+    benefit_types: ["rec3PfnqeqyxSbx1x"],
+    id: "recvzRaT9ormprNkb",
+    vac_name_en: "Disability Pension",
+    vac_name_fr: "Pension d'invalidité"
+  }
+];
+
+export const joinedBenefitsFixture = [
+  {
+    vac_name_en: "Disability Award",
+    vac_name_fr: "Prix ​​d'invalidité",
+    benefitTypeEn: "BT EN 1",
+    benefitTypeFr: "BT FR 1",
+    descriptionEn: "Money compensation for a service related injury",
+    descriptionFr: "Compensation monétaire pour une blessure liée au service",
+    linkEn: "English link",
+    linkFr: "French link"
+  },
+  {
+    vac_name_en: "Disability Pension",
+    vac_name_fr: "Pension d'invalidité",
+    benefitTypeEn: "BT EN 2",
+    benefitTypeFr: "BT FR 2",
+    descriptionEn: "A pension compensating service related injuries",
+    descriptionFr: "Une blessure compensant les blessures liées au service",
+    linkEn: "English link",
+    linkFr: "French link"
+  }
+];
