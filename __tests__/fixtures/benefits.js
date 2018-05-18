123d06735ec4f1c20c4277e494ed4032644cde11
Steve Astels <sastels@gmail.com>
2018-05-18 19:58:29 -0400

    fix benefit card tests

--- __tests__/fixtures/benefits.js
+++ __tests__/fixtures/benefits.js
@@ -1,39 +1,16 @@
-export const benefitsFixture = [
+const benefitsFixture = [
   {
-    patron_types: ["rec726lY5vUBEh2Sv"],
-    benefit_types: ["rec3PfnqeqyxSbx1x"],
-    id: "recQtMLSsyS90o4rV",
-    vac_name_en: "Disability Award",
-    vac_name_fr: "Prix ​​d'invalidité"
+    vacNameEn: "Disability Award",
+    vacNameFr: "Prix ​​d'invalidité",
+    benefitPageEn: "English link",
+    benefitPageFr: "French link"
   },
   {
-    patron_types: ["recDAuNt8DXhD88Mr"],
-    benefit_types: ["recQO4AHswOl75poF"],
-    id: "recvzRaT9ormprNkb",
-    vac_name_en: "Disability Pension",
-    vac_name_fr: "Pension d'invalidité"
+    vacNameEn: "Disability Pension",
+    vacNameFr: "Pension d'invalidité",
+    benefitPageEn: "English link",
+    benefitPageFr: "French link"
   }
 ];
 
-export const joinedBenefitsFixture = [
-  {
-    vac_name_en: "Disability Award",
-    vac_name_fr: "Prix ​​d'invalidité",
-    benefitTypeEn: "BT EN 1",
-    benefitTypeFr: "BT FR 1",
-    descriptionEn: "Money compensation for a service related injury",
-    descriptionFr: "Compensation monétaire pour une blessure liée au service",
-    linkEn: "English link",
-    linkFr: "French link"
-  },
-  {
-    vac_name_en: "Disability Pension",
-    vac_name_fr: "Pension d'invalidité",
-    benefitTypeEn: "BT EN 2",
-    benefitTypeFr: "BT FR 2",
-    descriptionEn: "A pension compensating service related injuries",
-    descriptionFr: "Une blessure compensant les blessures liées au service",
-    linkEn: "English link",
-    linkFr: "French link"
-  }
-];
+export default benefitsFixture;
