18c425ab9918ccc5b223e8443597eb88679efc21
Max Neuvians <max@neuvians.net>
2018-05-23 13:05:06 -0400

    Fixed small test in BB to bump % coverage

--- __tests__/components/BB_test.js
+++ __tests__/components/BB_test.js
@@ -1,23 +1,32 @@
 /* eslint-env jest */
 
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
 
 import { BB } from "../../components/BB";
 import benefitsFixture from "../fixtures/benefits";
 import elegibilityPathsFixture from "../fixtures/eligibilityPaths";
+import needsFixture from "../fixtures/needs";
 
 jest.mock("react-ga");
 
 describe("BB", () => {
   let props;
   let _mountedBB;
+  let _shallowBB;
 
   const mounted_BB = () => {
     if (!_mountedBB) {
-      _mountedBB = shallow(<BB {...props} />);
+      _mountedBB = mount(<BB {...props} />);
     }
     return _mountedBB;
+  };
+
+  const shallow_BB = () => {
+    if (!_shallowBB) {
+      _shallowBB = shallow(<BB {...props} />);
+    }
+    return _shallowBB;
   };
 
   beforeEach(() => {
@@ -43,59 +52,59 @@ describe("BB", () => {
         avatar: "BB-avatar-92"
       }
     };
-    _mountedBB = undefined;
+    _shallowBB = undefined;
   });
 
   it("has a correct filteredBenefits function", () => {
-    let BBInstance = mounted_BB().instance();
+    let BBInstance = shallow_BB().instance();
     expect(
       BBInstance.filteredBenefits(
         benefitsFixture,
         elegibilityPathsFixture,
         props.selectedEligibility,
-        props.needs,
+        needsFixture,
         props.selectedNeeds
       )
     ).toEqual([benefitsFixture[0]]);
   });
 
   it("has a serviceTypes filter", () => {
-    expect(mounted_BB().find("#serviceTypeFilter").length).toEqual(1);
+    expect(shallow_BB().find("#serviceTypeFilter").length).toEqual(1);
   });
   it("has a patronType filter", () => {
-    expect(mounted_BB().find("#patronTypeFilter").length).toEqual(1);
+    expect(shallow_BB().find("#patronTypeFilter").length).toEqual(1);
   });
 
   it("has the filters contained in a collapse component", () => {
     expect(
-      mounted_BB()
+      shallow_BB()
         .find("#collapseBlock")
         .find("#serviceTypeFilter").length
     ).toEqual(1);
     expect(
-      mounted_BB()
+      shallow_BB()
         .find("#collapseBlock")
         .find("#patronTypeFilter").length
     ).toEqual(1);
   });
 
   it("has the filter initially expanded", () => {
-    expect(mounted_BB().state().expanded).toEqual(true);
+    expect(shallow_BB().state().expanded).toEqual(true);
   });
 
   it("has a button to collapse / expand the filter", () => {
-    mounted_BB()
+    shallow_BB()
       .find("#expandButton")
       .simulate("click");
-    expect(mounted_BB().state().expanded).toEqual(false);
-    mounted_BB()
+    expect(shallow_BB().state().expanded).toEqual(false);
+    shallow_BB()
       .find("#expandButton")
       .simulate("click");
-    expect(mounted_BB().state().expanded).toEqual(true);
+    expect(shallow_BB().state().expanded).toEqual(true);
   });
 
   it("has the selected benefit cards", () => {
-    const benefitCards = mounted_BB().find(".BenefitCards");
+    const benefitCards = shallow_BB().find(".BenefitCards");
     expect(benefitCards.length).toEqual(1);
     benefitCards.map((bt, i) => {
       expect(bt.prop("benefit").vac_name_fr).toEqual(
@@ -104,9 +113,18 @@ describe("BB", () => {
     });
   });
 
+  it("allows a person to click on filters", () => {
+    let filter = mounted_BB()
+      .find("FilterSelector")
+      .first();
+    let checkbox = filter.find("Checkbox").first();
+    checkbox.simulate("change");
+    expect(props.toggleSelectedEligibility).toBeCalled();
+  });
+
   it("has an All Benefits Link", () => {
     expect(
-      mounted_BB()
+      shallow_BB()
         .find(".AllBenefits")
         .text()
     ).toEqual("Show All Benefits");
