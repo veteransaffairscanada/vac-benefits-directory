73c08f849780ece4064b2278d0dcdbd1414ffa1a
Max Neuvians <max@neuvians.net>
2018-05-23 14:33:56 -0400

    Fixed spacing on FilterSelector

--- components/filter_selector.js
+++ components/filter_selector.js
@@ -16,17 +16,32 @@ type Props = {
   handleChange: mixed
 };
 
+const checkboxStyle = {
+  margin: "-5px 0"
+};
+
+const fieldStyle = {
+  margin: "5px 0 15px 0"
+};
+
+const labelStyle = {
+  margin: "5px 0 10px 0"
+};
+
 class FilterSelector extends Component<Props> {
   render() {
     const { t } = this.props;
     return (
-      <FormControl component="fieldset">
-        <FormLabel component="legend">{t(this.props.legend)}</FormLabel>
+      <FormControl component="fieldset" style={fieldStyle}>
+        <FormLabel component="legend" style={labelStyle}>
+          {t(this.props.legend)}
+        </FormLabel>
         <FormGroup>
           {this.props.filters.map(pt => (
             <FormControlLabel
               id={pt.id}
               key={pt.id}
+              style={checkboxStyle}
               control={
                 <Checkbox
                   checked={this.props.selectedFilters.hasOwnProperty(pt.id)}
