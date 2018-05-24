c7dfaa5869adc5f1244408654b880dca782a447e
Steve Astels <sastels@gmail.com>
2018-05-24 11:22:25 -0400

    don't show Benefit Cards for benefits that need a gateway

--- __tests__/fixtures/benefits.js
+++ __tests__/fixtures/benefits.js
@@ -1,18 +1,20 @@
 const benefitsFixture = [
   {
-    id: "0",
+    id: "1",
     vacNameEn: "Disability Award",
     vacNameFr: "Prix ​​d'invalidité",
     benefitPageEn: "English link",
-    benefitPageFr: "French link"
+    benefitPageFr: "French link",
+    availableIndependently: "Requires Gateway Benefit"
   },
   {
-    id: "1",
+    id: "0",
     vacNameEn: "Disability Pension",
     vacNameFr: "Pension d'invalidité",
     benefitPageEn: "English link",
     benefitPageFr: "French link",
-    childBenefits: ["0"]
+    childBenefits: ["1"],
+    availableIndependently: "Independant"
   }
 ];
 
