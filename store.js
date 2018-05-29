e563bdc17ea213440a3f63fc7460b16c76a309a9
Steve Astels <sastels@gmail.com>
2018-05-29 15:49:17 -0400

    remove polyfill from store.js

--- store.js
+++ store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
-import "babel-polyfill/dist/polyfill";
 
 const exampleInitialState = {
   count: 10,
