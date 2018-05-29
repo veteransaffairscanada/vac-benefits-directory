f11fa90214ec90ef0c67144296723c9a3d04293f
smcmurtry <smcmurtry@gmail.com>
2018-05-29 13:36:53 -0400

    added benefit one liner to children

--- components/embedded_benefit_card.js
+++ components/embedded_benefit_card.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import { Typography, Button } from "material-ui";
+import { Typography, Button, Grid } from "material-ui";
 import classnames from "classnames";
 import { withStyles } from "material-ui/styles";
 import ExpansionPanel from "material-ui/ExpansionPanel/ExpansionPanel";
 import ExpansionPanelSummary from "material-ui/ExpansionPanel/ExpansionPanelSummary";
 import ExpansionPanelDetails from "material-ui/ExpansionPanel/ExpansionPanelDetails";
-import ExpansionPanelActions from "material-ui/ExpansionPanel/ExpansionPanelActions";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import PropTypes from "prop-types";
@@ -49,25 +48,35 @@ export class EmbeddedBenefitCard extends Component<Props> {
           </Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Typography variant="title">
+                {t("Benefit Description")}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                size="small"
+                target="_blank"
+                variant="raised"
+                href={
+                  this.props.t("current-language-code") === "en"
+                    ? benefit.benefitPageEn
+                    : benefit.benefitPageFr
+                }
+              >
+                {t("View Details")}
+              </Button>
+            </Grid>
+          </Grid>
         </ExpansionPanelDetails>
-        <ExpansionPanelActions>
-          <Button
-            size="small"
-            target="_blank"
-            variant="raised"
-            href={
-              this.props.t("current-language-code") === "en"
-                ? benefit.benefitPageEn
-                : benefit.benefitPageFr
-            }
-          >
-            {t("View Details")}
-          </Button>
-        </ExpansionPanelActions>
       </ExpansionPanel>
     );
   }
