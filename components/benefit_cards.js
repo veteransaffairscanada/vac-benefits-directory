40a1dc1e7914968f7ede21b238d399ec40e9fdd1
smcmurtry <smcmurtry@gmail.com>
2018-06-01 11:58:22 -0400

    work in progress

--- components/benefit_cards.js
+++ components/benefit_cards.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Grid, Typography, Button } from "material-ui";
 import { withStyles } from "material-ui/styles";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import AddIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+// import RemoveIcon from "@material-ui/icons/Remove";
 import EmbeddedBenefitCard from "./embedded_benefit_card";
 import ExpansionPanel from "material-ui/ExpansionPanel/ExpansionPanel";
 import ExpansionPanelSummary from "material-ui/ExpansionPanel/ExpansionPanelSummary";
@@ -95,7 +95,7 @@ export class BenefitCard extends Component {
             <ExpansionPanelSummary
               className={classes.ExpansionPanelSummary}
               expandIcon={<AddIcon />}
-              // expandIcon={<ExpandMoreIcon />}
+              // expandIcon={<RemoveIcon />}
               onClick={() => this.toggleOpenState()}
             >
               <div>
