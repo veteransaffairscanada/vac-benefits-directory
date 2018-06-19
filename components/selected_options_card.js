f3cd2bd962c90b637711977066688d2e191cb966
Max Neuvians <max@neuvians.net>
2018-06-19 12:11:51 +0200

    Fixed tests

--- components/selected_options_card.js
+++ components/selected_options_card.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Card, { CardContent, CardActions } from "@material-ui/core//Card";
-import Button from "@material-ui/core//Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 
-import { Typography } from "@material-ui/core/";
+import Typography from "@material-ui/core/Typography";
 
 class SelectedOptionsCard extends Component {
   render() {
--- pages/data-validation.js
+++ pages/data-validation.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import Table, {
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow
-} from "@material-ui/core/Table";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
 import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
 
