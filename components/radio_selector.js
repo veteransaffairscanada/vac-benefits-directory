9ba3357dcebd612997aa3e181ec4833b25f1a865
Max Neuvians <max@neuvians.net>
2018-06-21 13:57:35 +0200

    Material upgrade (#383)
    * WIP Material upgrade
    
    * Fixed tests
    
    * Cleaned up slashes
    
    Closes #381

--- components/radio_selector.js
+++ components/radio_selector.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "material-ui/styles";
-import { FormControl, FormControlLabel, FormLabel } from "material-ui/Form";
-import Radio from "material-ui/Radio";
-import RadioGroup from "material-ui/Radio/RadioGroup";
+import { withStyles } from "@material-ui/core/styles";
+import { FormControl, FormControlLabel, FormLabel } from "@material-ui/core";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 
 const styles = theme => ({
   formControl: {
