c074e35f42f3382438b8679e1c801a95d3afe39f
smcmurtry <smcmurtry@gmail.com>
2018-06-22 15:15:08 -0400

    got the ircc FederalBanner component working

---
+++ components/language_button.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Button } from "@material-ui/core";
+import { logEvent } from "../utils/analytics";
+import Router from "next/router";
+
+class LanguageButton extends Component {
+  changeLanguage = () => {
+    const newQuery = Router.query;
+    newQuery.lng = this.props.t("other-language-code");
+    Router.push({
+      pathname: Router.pathname,
+      query: newQuery
+    });
+    this.props.i18n.changeLanguage(this.props.t("other-language-code"));
+    logEvent("Language change", this.props.t("other-language"));
+  };
+
+  render() {
+    const { t } = this.props;
+
+    return (
+      <Button
+        id="changeLanguage"
+        onClick={this.changeLanguage}
+        style={{ color: "#fff" }}
+      >
+        {t("other-language")}
+      </Button>
+    );
+  }
+}
+
+LanguageButton.propTypes = {
+  i18n: PropTypes.object,
+  t: PropTypes.func
+};
+
+export default LanguageButton;
--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,7 @@ import ErrorBoundary from "../components/error_boundary";
 import Head from "../components/head";
 import FeedbackBar from "../components/feedbackBar";
 import Footer from "../components/footer";
-import MenuBar from "../components/menu_bar";
+import FederalBanner from "../components/federal_banner";
 import Noscript from "../components/noscript";
 
 import classnames from "classnames";
@@ -57,7 +57,7 @@ class Layout extends Component {
             <div className={classnames(this.props.classes.header)}>
               <Container>
                 <PhaseBanner alpha>{this.props.t("alpha")}</PhaseBanner>
-                <MenuBar
+                <FederalBanner
                   i18n={this.props.i18n}
                   t={this.props.t}
                   showRefreshCache={this.props.showRefreshCache}
