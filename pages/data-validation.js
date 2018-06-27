96adc2d8ccabfde38953a086532ce3655813cf86
VAC-ACC\APMCGUIG <Adam.McGuigan@vac-acc.gc.ca>
2018-06-27 11:37:25 -0300

    Checks Eligibility Path

--- pages/data-validation.js
+++ pages/data-validation.js
@@ -45,6 +45,10 @@ export class DataValidation extends Component {
     return !(b.needs && b.needs != "");
   }
 
+  checkEligibiltyPaths(b) {
+    return !(b.eligibilityPaths && b.eligibilityPaths != "");
+  }
+
   render() {
     const {
       i18n,
@@ -86,6 +90,11 @@ export class DataValidation extends Component {
         "Benefits Without Needs",
         benefits.filter(this.checkMissingNeeds).length,
         benefits.filter(this.checkMissingNeeds).length == 0 ? "Pass" : "Fail"
+      ),
+      this.createData(
+        "Benefits not in an Eligibility Path",
+        benefits.filter(this.checkEligibiltyPaths).length,
+        benefits.filter(this.checkEligibiltyPaths).length == 0 ? "Pass" : "Fail"
       )
     ];
 
