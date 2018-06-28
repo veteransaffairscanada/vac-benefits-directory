99415b3e140343292cc22382ba1e35a443729b62
Max Neuvians <max@neuvians.net>
2018-06-28 15:08:11 -0400

    Fixed URL bug

--- pages/_app.js
+++ pages/_app.js
@@ -8,31 +8,49 @@ import { initStore } from "../store";
 export default withRedux(initStore)(
   class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
+      let currentState = ctx.store.getState();
       if (ctx.req) {
         ctx.store.dispatch({ type: "LOAD_DATA", data: ctx.req.data });
-        if (ctx.query.patronType) {
-          ctx.store.dispatch({
-            type: "SET_PATRON_TYPE",
-            data: ctx.query.patronType
-          });
-        }
-        if (ctx.query.serviceType) {
-          ctx.store.dispatch({
-            type: "SET_SERVICE_TYPE",
-            data: ctx.query.serviceType
-          });
-        }
-        if (ctx.query.statusAndVitals) {
-          ctx.store.dispatch({
-            type: "SET_STATUS_TYPE",
-            data: ctx.query.statusAndVitals
-          });
-        }
-        if (ctx.query.selectedNeeds) {
-          let selectedNeeds = {};
-          ctx.query.selectedNeeds.split(",").forEach(id => {
-            selectedNeeds[id] = id;
-          });
+      }
+      if (
+        ctx.query.patronType &&
+        ctx.query.patronType !== currentState.patronType
+      ) {
+        ctx.store.dispatch({
+          type: "SET_PATRON_TYPE",
+          data: ctx.query.patronType
+        });
+      }
+
+      if (
+        ctx.query.serviceType &&
+        ctx.query.serviceType !== currentState.serviceType
+      ) {
+        ctx.store.dispatch({
+          type: "SET_SERVICE_TYPE",
+          data: ctx.query.serviceType
+        });
+      }
+
+      if (
+        ctx.query.statusAndVitals &&
+        ctx.query.statusAndVitals !== currentState.statusAndVitals
+      ) {
+        ctx.store.dispatch({
+          type: "SET_STATUS_TYPE",
+          data: ctx.query.statusAndVitals
+        });
+      }
+
+      if (ctx.query.selectedNeeds) {
+        let selectedNeeds = {};
+        ctx.query.selectedNeeds.split(",").forEach(id => {
+          selectedNeeds[id] = id;
+        });
+        if (
+          JSON.stringify(selectedNeeds) !==
+          JSON.stringify(currentState.selectedNeeds)
+        ) {
           ctx.store.dispatch({
             type: "SET_SELECTED_NEEDS",
             data: selectedNeeds
