e86b0113a6917460b45cc94c79a65d59f0ce7c84
Stephen McMurtry <smcmurtry@gmail.com>
2018-07-03 17:24:04 -0400

    Getting maps working (#472)

---
+++ components/area_office_map.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
+
+export class AreaOfficeMap extends Component {
+  state = {};
+  getToggleOpen(id) {
+    return () => {
+      const obj = {};
+      obj[id] = !this.state[id];
+      this.setState(obj);
+    };
+  }
+  componentWillMount() {
+    this.props.areaOffices.forEach(d => {
+      const obj = {};
+      obj[d.id] = false;
+      this.setState(obj);
+    });
+  }
+
+  render() {
+    const { t } = this.props;
+    return (
+      <GoogleMap
+        defaultZoom={4}
+        defaultCenter={{ lat: 52, lng: -90 }}
+        {...this.props}
+      >
+        {this.props.areaOffices.map((d, i) => {
+          return (
+            <Marker
+              key={i}
+              position={{ lat: +d.lat, lng: +d.lng }}
+              onClick={this.getToggleOpen(d.id)}
+            >
+              {this.state[d.id] ? (
+                <InfoBox>
+                  <div
+                    style={{
+                      fontSize: `16px`,
+                      fontColor: `black`,
+                      backgroundColor: "white",
+                      padding: "12px"
+                    }}
+                  >
+                    {d["name_" + t("current-language-code")]}
+                  </div>
+                </InfoBox>
+              ) : (
+                ""
+              )}
+            </Marker>
+          );
+        })}
+      </GoogleMap>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    areaOffices: state.areaOffices
+  };
+};
+
+AreaOfficeMap.propTypes = {
+  areaOffices: PropTypes.array.isRequired,
+  t: PropTypes.func.isRequired
+};
+
+export default withScriptjs(
+  withGoogleMap(connect(mapStateToProps)(AreaOfficeMap))
+);
