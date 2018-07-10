1fbcfb7fb800db76812b53d226a6018ec29b7f42
Max Neuvians <max@neuvians.net>
2018-07-10 11:40:02 -0400

    Selecting needs now jumps to the top of the page

--- components/needs_selector.js
+++ components/needs_selector.js
@@ -68,6 +68,7 @@ export class NeedsSelector extends Component {
       logEvent("FilterClick", "need", id);
       newSelectedNeeds[id] = id;
     }
+    window.scrollTo(0, 0);
     this.props.setSelectedNeeds(newSelectedNeeds);
   };
 
