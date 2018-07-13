bdf0fa237fbe4b3806ae5708252fd84ad58bf2d3
Steve Astels <sastels@gmail.com>
2018-07-13 09:48:46 -0400

    add padding around tags

--- components/need_tag.js
+++ components/need_tag.js
@@ -5,6 +5,7 @@ import { withStyles } from "@material-ui/core/styles";
 const styles = theme => ({
   needsTag: {
     marginRight: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
     color: "black",
     borderRadius: 1,
     display: "inline-flex",
