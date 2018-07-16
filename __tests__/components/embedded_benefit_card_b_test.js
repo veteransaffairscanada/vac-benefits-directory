f425fa2449e007fe748694c5ce1efc1a0dad9b6f
Steve Astels <sastels@gmail.com>
2018-07-16 16:23:51 -0400

    tests and padding

---
+++ __tests__/components/embedded_benefit_card_b_test.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { mount } from "enzyme";
+import { EmbeddedBenefitCard } from "../../components/embedded_benefit_card_b";
+import benefitsFixture from "../fixtures/benefits";
+
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
+describe("EmbeddedBenefitCard", () => {
+  let props;
+  let _mountedEmbeddedBenefitCard;
+  const mountedEmbeddedBenefitCard = () => {
+    if (!_mountedEmbeddedBenefitCard) {
+      _mountedEmbeddedBenefitCard = mount(<EmbeddedBenefitCard {...props} />);
+    }
+    return _mountedEmbeddedBenefitCard;
+  };
+
+  beforeEach(() => {
+    props = {
+      t: () => "en",
+      benefit: benefitsFixture[0],
+      classes: {},
+      onRef: foo => foo,
+      showFavourite: false
+    };
+    _mountedEmbeddedBenefitCard = undefined;
+  });
+
+  it("passes axe tests", async () => {
+    let html = mountedEmbeddedBenefitCard().html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+
+  it("has a blank target", () => {
+    expect(
+      mountedEmbeddedBenefitCard()
+        .find("Button")
+        .prop("target")
+    ).toEqual("_blank");
+  });
+
+  it("has expected English text and href", () => {
+    expect(mountedEmbeddedBenefitCard().text()).toContain(
+      benefitsFixture[0].vacNameEn
+    );
+    expect(
+      mountedEmbeddedBenefitCard()
+        .find("Button")
+        .prop("href")
+    ).toEqual(benefitsFixture[0].benefitPageEn);
+    expect(mountedEmbeddedBenefitCard().text()).toContain(
+      benefitsFixture[0].oneLineDescriptionEn
+    );
+  });
+
+  describe("when language is French", () => {
+    beforeEach(() => {
+      props.t = () => "fr";
+    });
+
+    it("has expected French text and href", () => {
+      expect(mountedEmbeddedBenefitCard().text()).toContain(
+        benefitsFixture[0].vacNameFr
+      );
+      expect(
+        mountedEmbeddedBenefitCard()
+          .find("Button")
+          .prop("href")
+      ).toEqual(benefitsFixture[0].benefitPageFr);
+      expect(mountedEmbeddedBenefitCard().text()).toContain(
+        benefitsFixture[0].oneLineDescriptionFr
+      );
+    });
+  });
+
+  it("Clicking the link logs an exit event", () => {
+    let analytics = require("../../utils/analytics");
+    analytics.logEvent = jest.fn();
+    mountedEmbeddedBenefitCard()
+      .find("Button")
+      .simulate("click");
+    expect(analytics.logEvent).toBeCalledWith(
+      "Exit",
+      benefitsFixture[1].benefitPageEn
+    );
+  });
+});
--- components/embedded_benefit_card_b.js
+++ components/embedded_benefit_card_b.js
@@ -5,17 +5,16 @@ import classnames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
 import { KeyboardBackspace } from "@material-ui/icons";
 import { logEvent } from "../utils/analytics";
-import { connect } from "react-redux";
 import Paper from "@material-ui/core/Paper";
 
 const styles = theme => ({
   root: {
     margin: "20px",
     marginTop: "0px",
-    padding: "10px"
+    padding: "20px",
+    paddingTop: "5px"
   },
   heading: {
-    size: "small",
     paddingLeft: "0px",
     paddingRight: "0px",
     textTransform: "none",
@@ -57,24 +56,10 @@ export class EmbeddedBenefitCard extends Component {
 
   componentDidMount() {
     this.props.onRef(this);
-    const needsMet = this.getNeedsMet();
-    if (needsMet.length > 0) {
-      this.setState({ open: true });
-    }
   }
 
   componentWillUnmount() {
     this.props.onRef(undefined);
-  }
-
-  getNeedsMet() {
-    return this.props.benefit.needs
-      ? this.props.needs.filter(
-          need =>
-            this.props.benefit.needs.indexOf(need.id) > -1 &&
-            this.props.selectedNeeds[need.id]
-        )
-      : [];
   }
 
   render() {
@@ -116,17 +101,8 @@ export class EmbeddedBenefitCard extends Component {
   }
 }
 
-const mapStateToProps = reduxState => {
-  return {
-    needs: reduxState.needs,
-    selectedNeeds: reduxState.selectedNeeds
-  };
-};
-
 EmbeddedBenefitCard.propTypes = {
   benefit: PropTypes.object.isRequired,
-  needs: PropTypes.array.isRequired,
-  selectedNeeds: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
   t: PropTypes.func.isRequired,
   onRef: PropTypes.func.isRequired,
@@ -134,6 +110,4 @@ EmbeddedBenefitCard.propTypes = {
   store: PropTypes.object
 };
 
-export default connect(mapStateToProps)(
-  withStyles(styles)(EmbeddedBenefitCard)
-);
+export default withStyles(styles)(EmbeddedBenefitCard);
