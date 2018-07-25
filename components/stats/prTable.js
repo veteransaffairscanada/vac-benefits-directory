d0c7eb515a3cfb16786cd1203c61f58db5dfa048
Max Neuvians <max@neuvians.net>
2018-07-25 19:53:30 -0400

    Split out table stats component

---
+++ components/stats/prTable.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Moment from "react-moment";
+import PropTypes from "prop-types";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+import { withStyles } from "@material-ui/core/styles/index";
+import { connect } from "react-redux";
+
+const styles = theme => ({
+  table: {
+    // width: 1000
+  }
+});
+
+export class PrTable extends Component {
+  filterMerged = () => {
+    return this.props.githubData.filter(pr => pr.merged_at);
+  };
+
+  render() {
+    const { classes, t } = this.props; // eslint-disable-line no-unused-vars
+
+    return (
+      <Table className={classes.table}>
+        <colgroup>
+          <col style={{ width: "30%" }} />
+          <col style={{ width: "40%" }} />
+          <col style={{ width: "30%" }} />
+        </colgroup>
+        <TableHead>
+          <TableRow>
+            <TableCell>Merged At</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>User</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.filterMerged().map((pr, i) => {
+            return (
+              <TableRow key={i}>
+                <TableCell>
+                  <Moment format="DD/MM/YYYY hh:mm A">{pr.merged_at}</Moment>
+                </TableCell>
+                <TableCell>{pr.title}</TableCell>
+                <TableCell>{pr.user.login}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    );
+  }
+}
+
+const mapStateToProps = reduxState => {
+  return {
+    githubData: reduxState.githubData
+  };
+};
+
+PrTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  githubData: PropTypes.array.isRequired,
+  t: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps)(
+  withStyles(styles, { withTheme: true })(PrTable)
+);
--- pages/stats.js
+++ pages/stats.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import Moment from "react-moment";
 import Paper from "@material-ui/core/Paper";
 import PropTypes from "prop-types";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
 
 import { withI18next } from "../lib/withI18next";
 import Layout from "../components/layout";
 
+import PrTable from "../components/stats/prTable";
+
 import { withStyles } from "@material-ui/core/styles/index";
-import { connect } from "react-redux";
 
 const styles = theme => ({
   root: {
@@ -27,10 +22,6 @@ const styles = theme => ({
 });
 
 export class Stats extends Component {
-  filterMerged = () => {
-    return this.props.githubData.filter(pr => pr.merged_at);
-  };
-
   render() {
     const { classes, i18n, t } = this.props; // eslint-disable-line no-unused-vars
 
@@ -42,37 +33,9 @@ export class Stats extends Component {
         showRefreshCache={false}
       >
         <div className={classes.root}>
-          <h1> {t("stats.title")} </h1>
+          <h1>{t("stats.title")}</h1>
           <Paper>
-            <Table className={classes.table}>
-              <colgroup>
-                <col style={{ width: "30%" }} />
-                <col style={{ width: "40%" }} />
-                <col style={{ width: "30%" }} />
-              </colgroup>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Merged At</TableCell>
-                  <TableCell>Title</TableCell>
-                  <TableCell>User</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.filterMerged().map((pr, i) => {
-                  return (
-                    <TableRow key={i}>
-                      <TableCell>
-                        <Moment format="DD/MM/YYYY hh:mm A">
-                          {pr.merged_at}
-                        </Moment>
-                      </TableCell>
-                      <TableCell>{pr.title}</TableCell>
-                      <TableCell>{pr.user.login}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
+            <PrTable t={this.props.t} />
           </Paper>
         </div>
       </Layout>
@@ -80,19 +43,10 @@ export class Stats extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    githubData: state.githubData
-  };
-};
-
 Stats.propTypes = {
-  githubData: PropTypes.array.isRequired,
   classes: PropTypes.object.isRequired,
   i18n: PropTypes.object.isRequired,
   t: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(
-  connect(mapStateToProps)(withI18next()(Stats))
-);
+export default withStyles(styles)(withI18next()(Stats));
