8255a1d96e2bde3787659897782aed7aeae979b4
apmcguigan <apmcguigan@upei.ca>
2018-07-25 12:00:38 -0300

    Button working, styling not 100%

--- components/profile_selector.js
+++ components/profile_selector.js
@@ -6,7 +6,7 @@ import classnames from "classnames";
 import RadioSelector from "./radio_selector";
 import { connect } from "react-redux";
 import "babel-polyfill/dist/polyfill";
-import { Grid } from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
 import {
   showStatusAndVitals,
   showServiceHealthIssue,
@@ -16,11 +16,35 @@ import {
 const styles = () => ({
   title: {
     color: "black !important"
+  },
+  clearButton: {
+    textTransform: "unset",
+    fontSize: "16px"
   }
 });
 
 export class ProfileSelector extends Component {
+  clearFilters = () => {
+    this.props.setPatronType("");
+    this.props.setServiceType("");
+    this.props.setStatusAndVitals("");
+    this.props.setServiceHealthIssue("");
+    this.props.setSelectedNeeds({});
+  };
+
   render() {
+    let filterSelected;
+    if (
+      this.props.selectedPatronType !== "" ||
+      this.props.selectedServiceType !== "" ||
+      this.props.selectedStatusAndVitals !== "" ||
+      this.props.selectedServiceHealthIssue != ""
+    ) {
+      filterSelected = 1;
+    } else {
+      filterSelected = 0;
+    }
+
     const {
       classes,
       t,
@@ -30,9 +54,34 @@ export class ProfileSelector extends Component {
     } = this.props;
     return (
       <div>
-        <Typography variant="subheading" className={classnames(classes.title)}>
-          {t("B3.Filter by eligibility")}
-        </Typography>
+        <Typography
+          variant="subheading"
+          className={classnames(classes.title)}
+        />
+        {JSON.stringify(this.props.selectedNeeds) !== "{}" ||
+        this.props.selectedPatronType !== "" ? (
+          <Grid item sm={12} className={classnames(classes.gridItemButton)}>
+            <Button
+              className={classnames(classes.clearButton)}
+              id="ClearFilters"
+              variant="text"
+              size="small"
+              color="primary"
+              onClick={() => {
+                this.clearFilters();
+              }}
+            >
+              {t("reset filters")}{" "}
+              {"(" +
+                (filterSelected +
+                  Object.values(this.props.selectedNeeds).length) +
+                ")"}
+            </Button>
+          </Grid>
+        ) : (
+          ""
+        )}
+        {t("B3.Filter by eligibility")}
         <Grid container spacing={8}>
           <Grid item xs={12} id="patronTypeFilter">
             <RadioSelector
@@ -92,12 +141,37 @@ export class ProfileSelector extends Component {
   }
 }
 
+const mapDispatchToProps1 = dispatch => {
+  return {
+    setPatronType: patronType => {
+      dispatch({ type: "SET_PATRON_TYPE", data: patronType });
+    },
+    setServiceType: serviceType => {
+      dispatch({ type: "SET_SERVICE_TYPE", data: serviceType });
+    },
+    setStatusAndVitals: statusType => {
+      dispatch({ type: "SET_STATUS_TYPE", data: statusType });
+    },
+    setServiceHealthIssue: serviceHealthIssue => {
+      dispatch({ type: "SET_HEALTH_ISSUE", data: serviceHealthIssue });
+    },
+    setSelectedNeeds: needsObject => {
+      dispatch({ type: "SET_SELECTED_NEEDS", data: needsObject });
+    }
+  };
+};
+
 const mapStateToProps = reduxState => {
   return {
     statusAndVitals: reduxState.statusAndVitals,
     showStatusAndVitals: showStatusAndVitals(reduxState),
     showServiceHealthIssue: showServiceHealthIssue(reduxState),
-    showServiceType: showServiceType(reduxState)
+    showServiceType: showServiceType(reduxState),
+    selectedNeeds: reduxState.selectedNeeds,
+    selectedPatronType: reduxState.patronType,
+    selectedServiceType: reduxState.serviceType,
+    selectedStatusAndVitals: reduxState.statusAndVitals,
+    selectedServiceHealthIssue: reduxState.serviceHealthIssue
   };
 };
 
@@ -109,9 +183,20 @@ ProfileSelector.propTypes = {
   store: PropTypes.object,
   showStatusAndVitals: PropTypes.bool.isRequired,
   showServiceHealthIssue: PropTypes.bool.isRequired,
-  showServiceType: PropTypes.bool.isRequired
+  showServiceType: PropTypes.bool.isRequired,
+  setPatronType: PropTypes.func.isRequired,
+  setServiceType: PropTypes.func.isRequired,
+  setStatusAndVitals: PropTypes.func.isRequired,
+  setServiceHealthIssue: PropTypes.func.isRequired,
+  setSelectedNeeds: PropTypes.func.isRequired,
+  selectedNeeds: PropTypes.object.isRequired,
+  selectedPatronType: PropTypes.string.isRequired,
+  selectedServiceType: PropTypes.string.isRequired,
+  selectedStatusAndVitals: PropTypes.string.isRequired,
+  selectedServiceHealthIssue: PropTypes.string.isRequired
 };
 
-export default connect(mapStateToProps)(
-  withStyles(styles, { withTheme: true })(ProfileSelector)
-);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps1
+)(withStyles(styles, { withTheme: true })(ProfileSelector));
