ef3d4330081bd83be39a744c24ea4284ebc50843
Max Neuvians <max@neuvians.net>
2018-08-07 09:17:09 -0400

    condensed spaces

--- pages/data-validation.js
+++ pages/data-validation.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-
 import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
-
 import ReactMoment from "react-moment";
-
 import { withI18next } from "../lib/withI18next";
 import Layout from "../components/layout";
 import { connect } from "react-redux";
