375f1a03a95735646d58f357edbdebdc6041fdc2
Steve Astels <sastels@gmail.com>
2018-08-08 09:28:35 -0400

    print working from BB without Link

--- components/BB.js
+++ components/BB.js
@@ -131,7 +131,9 @@ export class BB extends Component {
     selectedEligibility,
     selectedNeeds,
     sortby,
-    language
+    language,
+    closestAreaOffice,
+    selectedAreaOffice
   ) => {
     const filteredBenefitsIDs = filteredBenefits.map(b => b.id);
     const needsIDs = Object.keys(selectedNeeds);
@@ -150,6 +152,12 @@ export class BB extends Component {
     if (filteredBenefitsIDs.length > 0) {
       url += "&benefits=" + filteredBenefitsIDs.join(",");
     }
+    if (closestAreaOffice.id !== undefined) {
+      url += "&closestAOID=" + closestAreaOffice.id;
+    }
+    if (selectedAreaOffice.id !== undefined) {
+      url += "&selectedAOID=" + selectedAreaOffice.id;
+    }
     return url;
   };
 
@@ -163,7 +171,9 @@ export class BB extends Component {
       this.props.selectedEligibility,
       this.props.selectedNeeds,
       this.state.sortByValue,
-      t("current-language-code")
+      t("current-language-code"),
+      this.props.closestAreaOffice,
+      this.props.selectedAreaOffice
     );
 
     return (
@@ -200,19 +210,18 @@ export class BB extends Component {
                       ")"}
                   </Button>
                 </Link>
-                <Link href={printUrl}>
-                  <Button
-                    variant="flat"
-                    size="large"
-                    target="dan"
-                    className={classes.buttonBarButton}
-                    id="printButton"
-                  >
-                    <Print style={{ fontSize: "20px" }} />
-                    &nbsp;
-                    {pageWidth > 600 ? t("Print") : ""}
-                  </Button>
-                </Link>
+                <Button
+                  href={printUrl}
+                  variant="flat"
+                  size="large"
+                  target="print_page"
+                  className={classes.buttonBarButton}
+                  id="printButton"
+                >
+                  <Print style={{ fontSize: "20px" }} />
+                  &nbsp;
+                  {pageWidth > 600 ? t("Print") : ""}
+                </Button>
               </Grid>
               <Grid item xs={12} md={3}>
                 <TextField
@@ -324,7 +333,9 @@ const mapStateToProps = (reduxState, props) => {
       serviceHealthIssue: reduxState.serviceHealthIssue
     },
     selectedNeeds: reduxState.selectedNeeds,
-    pageWidth: reduxState.pageWidth
+    pageWidth: reduxState.pageWidth,
+    selectedAreaOffice: reduxState.selectedAreaOffice,
+    closestAreaOffice: reduxState.closestAreaOffice
   };
 };
 
@@ -343,7 +354,9 @@ BB.propTypes = {
   t: PropTypes.func.isRequired,
   favouriteBenefits: PropTypes.array.isRequired,
   store: PropTypes.object,
-  pageWidth: PropTypes.number.isRequired
+  pageWidth: PropTypes.number.isRequired,
+  selectedAreaOffice: PropTypes.object.isRequired,
+  closestAreaOffice: PropTypes.object.isRequired
 };
 
 export default connect(
