767dc96e0fd877b9091c66ee0d7568cd67300999
Steve Astels <sastels@gmail.com>
2018-08-10 11:14:47 -0400

    now clicking on inputs!

--- testcafe/index_page.test.js
+++ testcafe/index_page.test.js
@@ -1,20 +1,37 @@
 import { Selector } from "testcafe";
-// import { waitForReact } from 'testcafe-react-selectors';
+import { waitForReact } from "testcafe-react-selectors";
 
 const BDSelector = Selector("#BB");
+const serviceMemberSelector = Selector("input").withAttribute(
+  "value",
+  "service-person"
+);
+const CAFSelector = Selector("input").withAttribute("value", "CAF");
+const releasedSelector = Selector("input").withAttribute(
+  "value",
+  "releasedAlive"
+);
+const healthIssueSelector = Selector("input").withAttribute("value", "true");
+const firstNeedSelector = Selector("button").nth(4);
 
-// fixture("Guided Experience").page("https://cdsvacpoc.azurewebsites.net/");
-
-fixture("Guided Experience").page("http://localhost:3000/");
-// .beforeEach(async () => {
-//   await waitForReact();
-// });
+fixture("Guided Experience")
+  .page("http://localhost:3000/")
+  .beforeEach(async () => {
+    await waitForReact();
+  });
 
 test("Can click throught GE and get to BD", async t => {
   await t
-    .setPageLoadTimeout(50000)
     .click("#heroGuidedLink")
+    .click(serviceMemberSelector)
     .click("#nextButton")
+    .click(CAFSelector)
+    .click("#nextButton")
+    .click(releasedSelector)
+    .click("#nextButton")
+    .click(healthIssueSelector)
+    .click("#nextButton")
+    .click(firstNeedSelector)
     .click("#nextButton")
     .expect(BDSelector.exists)
     .ok();
