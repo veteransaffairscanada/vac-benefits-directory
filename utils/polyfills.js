0f7ce40837972a2a5eaec8d83dcd79832f5a06c3
Max Neuvians <max@neuvians.net>
2018-08-16 12:42:38 -0400

    Update polyfills.js

--- utils/polyfills.js
+++ utils/polyfills.js
@@ -3,6 +3,6 @@
 // Add polyfills here. Check all available here: https://github.com/zloirock/core-js
 // This files runs at the very beginning (even before React and Next.js core)
 
-// FOLLOWING IS THE IMPORT I NEEDED FOR IE 11, 10
+// FOLLOWING IS THE IMPORT NEEDED FOR IE 11, 10
 import "core-js/fn/object/assign";
 import "core-js/fn/object/entries";
