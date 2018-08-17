cde29e069ae99a0d33a58d795c9d73653195e158
Stephen McMurtry <smcmurtry@gmail.com>
2018-08-17 10:45:26 -0400

    Added emotion to guided experience (#940)

--- components/guided_experience.js
+++ components/guided_experience.js
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Grid } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles/index";
 import Button from "@material-ui/core/Button";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 import ArrowForward from "@material-ui/icons/ArrowForward";
 import Typography from "@material-ui/core/Typography";
-import classnames from "classnames";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { connect } from "react-redux";
-import styled from "react-emotion";
+import styled, { css } from "react-emotion";
+import { globalTheme } from "../theme";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       light: "#1A237E",
       main: "#E8EAF6",
-      dark: "#0000FF", // '#002884',
+      dark: "#0000FF",
       contrastText: "#fff"
     }
   },
@@ -30,49 +29,56 @@ const BlueBar = styled("div")`
   margin-bottom: 40px;
 `;
 
-const styles = () => ({
-  root: {
-    border: "solid 1px grey",
-    backgroundColor: "white",
-    marginTop: "5px",
-    margin: "15px"
-  },
-  box: {
-    padding: "20px"
-  },
-  container: {
-    margin: "0 auto",
-    maxWidth: "1200px",
-    paddingLeft: "16px",
-    paddingRight: "16px"
-  },
-  prevButton: {
-    marginTop: "20px",
-    marginLeft: "15px"
-  },
-  nextButton: {
-    backgroundColor: "black",
-    color: "white",
-    marginTop: 0,
-    margin: "25px",
-    "&:hover": {
-      background: "#606060"
-    }
-  },
-  title: {
-    fontSize: "1.5em",
-    color: "black"
-  },
-  subTitle: {
-    fontSize: "1em"
-  },
-  jumpButton: {
-    fontSize: "1.5em"
-  },
-  comma: {
-    marginRight: "0.5em"
+const root = css`
+  border: solid 1px grey;
+  background-color: white;
+  margin: 15px;
+`;
+
+const box = css`
+  padding: 20px;
+`;
+
+const container = css`
+  margin: 0 auto;
+  max-width: ${globalTheme.maxWidth};
+  padding-left: 16px;
+  padding-right: 16px;
+`;
+
+const prevButton = css`
+  margin-top: 20px !important;
+  margin-left: 15px !important;
+  text-transform: none;
+`;
+
+const nextButton = css`
+  background-color: black !important;
+  color: white;
+  margin-top: 0 !important;
+  margin: 25px !important;
+  text-transform: none;
+  &:hover {
+    background: #606060 !important;
   }
-});
+`;
+
+const title = css`
+  font-size: 1.5em !important;
+  color: black;
+`;
+
+const subTitle = css`
+  font-size: 1em !important;
+`;
+
+const jumpButton = css`
+  font-size: 1.5em;
+`;
+
+const comma = css`
+  margin-right: 0.5em;
+`;
 
 export class GuidedExperience extends Component {
   sectionMap = {
@@ -83,15 +89,14 @@ export class GuidedExperience extends Component {
   };
 
   render() {
-    const { t, classes, selectedEligibility } = this.props;
+    const { t, selectedEligibility } = this.props;
     const eligibilityKeys = Object.keys(selectedEligibility);
 
     return (
       <MuiThemeProvider theme={theme}>
-        <div id="guidedExperience" className={classes.container}>
+        <div id="guidedExperience" className={container}>
           <Button
             size="medium"
-            style={{ textTransform: "none" }}
             href={
               this.props.prevSection === "index"
                 ? this.props.indexURL
@@ -102,15 +107,15 @@ export class GuidedExperience extends Component {
                 ? undefined
                 : () => this.props.setSection(this.props.prevSection)
             }
-            className={classnames(classes.prevButton)}
+            className={prevButton}
           >
             <ArrowBack />
             &nbsp; &nbsp; {t("back")}
           </Button>
-          <div className={classnames(classes.root)}>
-            <Grid container spacing={24} className={classnames(classes.box)}>
+          <div className={root}>
+            <Grid container spacing={24} className={box}>
               <Grid item xs={12} md={3}>
-                <Typography className={classnames(classes.title)}>
+                <Typography className={title}>
                   {t("B3.Filter by eligibility")}
                 </Typography>
               </Grid>
@@ -133,12 +138,10 @@ export class GuidedExperience extends Component {
 
                     return (
                       <span key={i}>
-                        <span className={classes.comma}>
-                          {i === 0 ? "" : ","}
-                        </span>
+                        <span className={comma}>{i === 0 ? "" : ","}</span>
                         <a
                           id={"jumpButton" + i}
-                          className={classes.jumpButton}
+                          className={jumpButton}
                           href="#"
                           onClick={() =>
                             this.props.setSection(this.sectionMap[k])
@@ -154,7 +157,7 @@ export class GuidedExperience extends Component {
 
               <Grid item xs={12}>
                 <BlueBar />
-                <Typography className={classnames(classes.subTitle)}>
+                <Typography className={subTitle}>
                   {this.props.subtitle}
                 </Typography>
               </Grid>
@@ -167,7 +170,6 @@ export class GuidedExperience extends Component {
             <Button
               id="nextButton"
               size="medium"
-              style={{ textTransform: "none" }}
               href={
                 this.props.nextSection === "benefits-directory"
                   ? this.props.benefitsDirectoryUrl
@@ -178,7 +180,7 @@ export class GuidedExperience extends Component {
                   ? undefined
                   : () => this.props.setSection(this.props.nextSection)
               }
-              className={classnames(classes.nextButton)}
+              className={nextButton}
             >
               {t("next")} &nbsp; &nbsp;
               <ArrowForward />
@@ -203,7 +205,6 @@ const mapStateToProps = reduxState => {
 
 GuidedExperience.propTypes = {
   id: PropTypes.string.isRequired,
-  classes: PropTypes.object.isRequired,
   nextSection: PropTypes.string.isRequired,
   prevSection: PropTypes.string,
   t: PropTypes.func.isRequired,
@@ -217,4 +218,4 @@ GuidedExperience.propTypes = {
   indexURL: PropTypes.string
 };
 
-export default connect(mapStateToProps)(withStyles(styles)(GuidedExperience));
+export default connect(mapStateToProps)(GuidedExperience);
