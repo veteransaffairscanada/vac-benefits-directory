c2be50815b83acba80a819db058da8b61049083b
Max Neuvians <max@neuvians.net>
2018-08-18 17:00:02 -0400

    Updated pages/map tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,11 @@
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() });
+
+const position = { coords: { latitude: 10, longitude: 10 } };
+const mockGeolocation = {
+  getCurrentPosition: async success => success(position)
+};
+
+global.navigator.geolocation = mockGeolocation;
