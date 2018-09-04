c529f27a8728a4d174e8c64471cc99f3a53baf8e
Steve Astels <sastels@users.noreply.github.com>
2018-09-04 16:43:30 -0400

    colours from style guide (#1120)

--- theme.js
+++ theme.js
@@ -16,6 +16,18 @@ const theme = {
     sm: "960px",
     md: "1280px",
     lg: "1920px"
+  },
+  colour: {
+    paleGrey: "#eaebed",
+    warmGrey: "#979797",
+    brownishGrey: "#646464",
+    greyishBrownTwo: "#505050",
+    greyishBrown: "#434343",
+    cerulean: "#006cc9",
+    darkGreyBlue: "#284162",
+    fernGreen: "#39824d",
+    darkGreen: "#295f38",
+    tornadoRed: "#ea4335"
   }
 };
 
