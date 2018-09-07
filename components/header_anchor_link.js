e6637fc879148415cd877fb58b19a63e30675110
Steve Astels <sastels@users.noreply.github.com>
2018-09-07 08:40:18 -0400

    Tweak header anchor link (#1144)

--- components/header_anchor_link.js
+++ components/header_anchor_link.js
@@ -6,6 +6,7 @@ import ArrowBack from "@material-ui/icons/ArrowBack";
 import ArrowForward from "@material-ui/icons/ArrowForward";
 import Bookmark from "@material-ui/icons/Bookmark";
 import Print from "@material-ui/icons/Print";
+import CloseIcon from "@material-ui/icons/Close";
 
 const style = css`
   display: inline-block;
@@ -16,13 +17,18 @@ const style = css`
   text-decoration: none !important;
   :hover {
     text-decoration: underline !important;
+    cursor: pointer;
   }
   svg {
     margin-top: -4px;
     vertical-align: middle;
   }
 `;
-
+const closeIcon = css`
+  font-size: 100% !important;
+  margin-left: ${globalTheme.unit};
+  font-weight: bold;
+`;
 const nonMobileStyle = css`
   @media only screen and (max-width: ${globalTheme.max.xs}) {
     display: none;
@@ -31,16 +37,21 @@ const nonMobileStyle = css`
 
 class HeaderAnchorLink extends Component {
   render() {
-    const { id, icon, className, children, nonMobile } = this.props;
+    const { id, icon, className, children, nonMobile, onClick } = this.props;
     let propsCopy = JSON.parse(JSON.stringify(this.props));
     delete propsCopy.className;
     delete propsCopy.children;
     delete propsCopy.icon;
     delete propsCopy.nonMobile;
     delete propsCopy.id;
-
+    delete propsCopy.onClick;
     return (
-      <a className={cx(style, className)} id={"a-" + id} {...propsCopy}>
+      <a
+        className={cx(style, className)}
+        id={"a-" + id}
+        onClick={onClick}
+        {...propsCopy}
+      >
         {icon === "arrowBack" ? <ArrowBack /> : null}
         {icon === "bookmark" ? <Bookmark /> : null}
         {icon === "print" ? <Print /> : null}
@@ -49,6 +60,7 @@ class HeaderAnchorLink extends Component {
           <span className={nonMobileStyle}> {nonMobile} </span>
         ) : null}
         {icon === "arrowForward" ? <ArrowForward /> : null}
+        {icon === "close" ? <CloseIcon className={closeIcon} /> : null}
       </a>
     );
   }
@@ -63,7 +75,8 @@ HeaderAnchorLink.propTypes = {
   className: PropTypes.string,
   icon: PropTypes.string,
   label: PropTypes.object,
-  nonMobile: PropTypes.string
+  nonMobile: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default HeaderAnchorLink;
