6c11d890b8894f36195f85997f4cdcc11a3521a2
Steve Astels <sastels@users.noreply.github.com>
2018-09-24 15:16:32 -0400

    Security update (#1179)

--- __tests__/components/noscript_test.js
+++ __tests__/components/noscript_test.js
@@ -6,12 +6,6 @@ const { axe, toHaveNoViolations } = require("jest-axe");
 expect.extend(toHaveNoViolations);
 
 describe("Test Noscript component", () => {
-  it("displays a message inside a noscript tag", () => {
-    const noscript = mount(<Noscript t={key => key} />);
-
-    expect(noscript.find("div.copy p").text()).toEqual("noscript");
-  });
-
   it("passes axe tests", async () => {
     let html = mount(<Noscript t={key => key} />).html();
     expect(await axe(html)).toHaveNoViolations();
