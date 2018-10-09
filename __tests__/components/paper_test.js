b2586cb224e0f92b0cd17db67600378845cb74a1
smcmurtry <smcmurtry@gmail.com>
2018-10-09 16:51:43 -0400

    added test

---
+++ __tests__/components/paper_test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { mount } from "enzyme";
+import Paper from "../../components/paper";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
+describe("Container", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      children: <div id="test_div">hello</div>
+    };
+  });
+
+  it("passes axe tests", async () => {
+    let html = mount(<Paper {...props} />).html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+
+  it("shows children", () => {
+    expect(mount(<Paper {...props} />).find("#test_div").length).toEqual(1);
+  });
+});
