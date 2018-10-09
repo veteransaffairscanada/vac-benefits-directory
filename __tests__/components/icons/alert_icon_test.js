4afc3f6617f7f2e7ce733e0ee6b016e685ce966b
Stephen McMurtry <smcmurtry@gmail.com>
2018-10-09 11:03:19 -0400

    Added the new alert icon (#1262)

---
+++ __tests__/components/icons/alert_icon_test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { mount } from "enzyme";
+import AlertIcon from "../../../components/icons/alert_icon";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
+describe("AlertIcon", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      height: "30px"
+    };
+  });
+
+  it("renders", async () => {
+    mount(<AlertIcon {...props} />);
+  });
+
+  it("passes axe tests", async () => {
+    let html = mount(<AlertIcon {...props} />).html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+});
--- components/benefit_card_header.js
+++ components/benefit_card_header.js
@@ -1,28 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 import { connect } from "react-redux";
 import { css } from "react-emotion";
 import { globalTheme } from "../theme";
 import CardHeaderParentInfo from "./card_header_parent_info";
 import CardHeaderImportantInfo from "./card_header_important_info";
+import AlertIcon from "./icons/alert_icon";
 
 const cardTop = css`
   border-bottom: 1px solid #8b8b8b;
   padding-bottom: 15px;
   margin-bottom: 15px;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-`;
-const parentIcon = css`
-  margin-right: 15px;
-  font-size: 40px !important;
-  transform: scale(0.9);
-  color: ${globalTheme.colour.greyishBrown};
 `;
 const headerDesc = css`
   flex-grow: 1;
+  width: 100%;
+  box-sizing: border-box;
+  margin-left: 20px;
   color: ${globalTheme.colour.greyishBrown};
   @media only screen and (max-width: ${globalTheme.max.mobile}) {
     font-size: 12px;
@@ -47,8 +44,8 @@ export class BenefitCardHeader extends Component {
     if (includeParentInfo || includeImportantInfo) {
       return (
         <div className={cardTop}>
-          <ErrorOutlineIcon className={parentIcon} />
-          <span className={headerDesc}>
+          <AlertIcon />
+          <div className={headerDesc}>
             {includeParentInfo ? (
               <CardHeaderParentInfo
                 t={t}
@@ -59,7 +56,7 @@ export class BenefitCardHeader extends Component {
             {includeImportantInfo ? (
               <CardHeaderImportantInfo t={t} benefit={benefit} />
             ) : null}
-          </span>
+          </div>
         </div>
       );
     } else {
