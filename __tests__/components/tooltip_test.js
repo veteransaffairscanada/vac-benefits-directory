1f3a2301baecace8ea495d228d081996956379ef
smcmurtry <smcmurtry@gmail.com>
2018-10-10 17:20:12 -0400

    added test

---
+++ __tests__/components/tooltip_test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { mount } from "enzyme";
+import Tooltip from "../../components/tooltip";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
+describe("Tooltip", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      children: <div id="test_div">hello</div>,
+      showTooltip: true,
+      tooltipText: "tooltip text"
+    };
+  });
+
+  it("passes axe tests", async () => {
+    let html = mount(<Tooltip {...props} />).html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+
+  it("shows children", () => {
+    expect(mount(<Tooltip {...props} />).find("#test_div").length).toEqual(1);
+  });
+
+  it("shows tooltip if showTooltip = true", () => {
+    expect(mount(<Tooltip {...props} />).find("span").length).toEqual(1);
+  });
+
+  it("does not show tooltip if showTooltip = false", () => {
+    props.showTooltip = false;
+    expect(mount(<Tooltip {...props} />).find("span").length).toEqual(0);
+  });
+});
