838a1e47e1649a7ab7a0ed7e70665b4df15abf48
Steve Astels <sastels@gmail.com>
2018-10-10 11:38:16 -0400

    redid changes on top of master

--- components/favourites.js
+++ components/favourites.js
@@ -7,7 +7,7 @@ import { getPrintUrl } from "../selectors/urls";
 import Bookmark from "@material-ui/icons/BookmarkBorder";
 import Print from "@material-ui/icons/Print";
 import Link from "next/link";
-import { css, cx } from "react-emotion";
+import { css } from "react-emotion";
 import Container from "./container";
 import Header1 from "./typography/header1";
 import Header2 from "./typography/header2";
@@ -42,14 +42,28 @@ const outerGrid = css`
   padding-left: 16px;
   padding-right: 16px;
 `;
-const buttons = css`
-  margin-top: 30px !important;
-`;
 const topMatter = css`
   margin-bottom: 25px !important;
 `;
 const noBottomMargin = css`
   margin-bottom: 0px;
+`;
+const outerDiv = css`
+  padding-bottom: 16px !important;
+`;
+const topPadding = css`
+  padding-top: 30px;
+`;
+const container2 = css`
+  margin-right: 15px;
+  margin-left: 15px;
+`;
+const whiteBanner = css`
+  background-color: #fff;
+  border-bottom: solid 1px lightgrey;
+  width: 100%;
+  padding-bottom: 20px;
+  margin-bottom: 30px;
 `;
 
 export class Favourites extends Component {
@@ -93,121 +107,132 @@ export class Favourites extends Component {
     );
 
     return (
-      <Container id="favourites">
-        <Grid className={outerGrid} container spacing={24}>
-          <Grid item xs={6} className={buttons}>
-            <HeaderButton
-              id="backButton"
-              useLink
-              href={this.get_link("/benefits-directory")}
-              arrow="back"
-            >
-              {t("favourites.back_link")}
-            </HeaderButton>
-          </Grid>
-          <Grid item xs={6} className={cx(buttons, right)}>
-            <HeaderButton
-              href={this.props.printUrl}
-              target="print_page"
-              id="printButton"
-            >
-              <Print /> {t("Print")}
-            </HeaderButton>
-          </Grid>
+      <div className={outerDiv}>
+        <div className={whiteBanner}>
+          <Container className={topPadding}>
+            <div className={container2}>
+              <Grid container spacing={24}>
+                <Grid item xs={6}>
+                  <HeaderButton
+                    id="backButton"
+                    useLink
+                    href={this.get_link("/benefits-directory")}
+                    arrow="back"
+                  >
+                    {t("favourites.back_link")}
+                  </HeaderButton>
+                </Grid>
 
-          <Grid item xs={12} className={topMatter}>
-            <Header1 className={"BenefitsCounter"}>
-              {t("favourites.saved_benefits", { x: filteredBenefits.length })}
-            </Header1>
-          </Grid>
-          <Grid item md={8} xs={12}>
-            {this.state.showDisabledCookieBanner ? (
-              <DisabledCookiesBanner
-                t={t}
-                onClose={() =>
-                  this.setState({ showDisabledCookieBanner: false })
-                }
-              />
-            ) : null}
+                <Grid item xs={6} className={right}>
+                  <HeaderButton
+                    href={this.props.printUrl}
+                    target="print_page"
+                    id="printButton"
+                  >
+                    <Print /> {t("Print")}
+                  </HeaderButton>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
 
-            <Grid container spacing={24}>
-              <BenefitList
-                t={t}
-                filteredBenefits={filteredBenefits}
-                sortByValue={this.props.sortBy}
-                showFavourites={true}
-                searchString=""
-                store={this.props.store}
-                favouriteBenefits={this.props.favouriteBenefits}
-              />
+        <Container id="favourites">
+          <Grid className={outerGrid} container spacing={24}>
+            <Grid item xs={12} className={topMatter}>
+              <Header1 className={"BenefitsCounter"}>
+                {t("favourites.saved_benefits", { x: filteredBenefits.length })}
+              </Header1>
             </Grid>
-            {filteredBenefits.length == 0 ? (
-              <Body className={emptyList}>
-                <Bookmark className={bookmarkCSS} />
-                <br />
-                {t("favourites.help_msg_line1")}
-                <br />
-                <Link href={t("favourites.help_url")}>
-                  <a>{t("favourites.help_url_text")}</a>
-                </Link>
-                {" " + t("favourites.help_msg_line_connect") + " "}
-                <strong>{t("favourites.help_msg_emphasis") + " "}</strong>
-                {t("favourites.help_msg_last")}
-              </Body>
-            ) : (
-              ""
-            )}
+            <Grid item md={8} xs={12}>
+              {this.state.showDisabledCookieBanner ? (
+                <DisabledCookiesBanner
+                  t={t}
+                  onClose={() =>
+                    this.setState({ showDisabledCookieBanner: false })
+                  }
+                />
+              ) : null}
+
+              <Grid container spacing={24}>
+                <BenefitList
+                  t={t}
+                  filteredBenefits={filteredBenefits}
+                  sortByValue={this.props.sortBy}
+                  showFavourites={true}
+                  searchString=""
+                  store={this.props.store}
+                  favouriteBenefits={this.props.favouriteBenefits}
+                />
+              </Grid>
+              {filteredBenefits.length == 0 ? (
+                <Body className={emptyList}>
+                  <Bookmark className={bookmarkCSS} />
+                  <br />
+                  {t("favourites.help_msg_line1")}
+                  <br />
+                  <Link href={t("favourites.help_url")}>
+                    <a>{t("favourites.help_url_text")}</a>
+                  </Link>
+                  {" " + t("favourites.help_msg_line_connect") + " "}
+                  <strong>{t("favourites.help_msg_emphasis") + " "}</strong>
+                  {t("favourites.help_msg_last")}
+                </Body>
+              ) : (
+                ""
+              )}
+            </Grid>
+            <Grid item md={4} xs={12}>
+              <Paper padding="sm" className={contactUs}>
+                <Header2>{t("favourites.contact_us")}</Header2>
+                <p>
+                  <Link href={this.get_link("map")}>
+                    <a>{t("favourites.visit_prompt")}</a>
+                  </Link>
+                </p>
+
+                <Body>{t("favourites.print_instructions")}</Body>
+
+                <hr />
+
+                <p>
+                  <a href={"tel:" + t("contact.phone")}>{t("contact.phone")}</a>
+                </p>
+
+                <Body>{t("favourites.call_time")}</Body>
+
+                <hr />
+
+                <p>
+                  <a href={"mailto:" + t("contact.email")}>
+                    {t("contact.email")}
+                  </a>
+                </p>
+
+                <Body>{t("favourites.email_disclaimer")}</Body>
+
+                <hr />
+
+                <Header2 className={contactUsTitle}>
+                  {t("favourites.apply_prompt")}
+                </Header2>
+
+                <Body className={noBottomMargin}>
+                  <a
+                    href={t("contact.my_vac_link")}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t("favourites.login_link")}
+                  </a>
+                  &nbsp;
+                  {t("favourites.login_prompt")}
+                </Body>
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item md={4} xs={12}>
-            <Paper padding="sm" className={contactUs}>
-              <Header2>{t("favourites.contact_us")}</Header2>
-              <p>
-                <Link href={this.get_link("map")}>
-                  <a>{t("favourites.visit_prompt")}</a>
-                </Link>
-              </p>
-
-              <Body>{t("favourites.print_instructions")}</Body>
-
-              <hr />
-
-              <p>
-                <a href={"tel:" + t("contact.phone")}>{t("contact.phone")}</a>
-              </p>
-
-              <Body>{t("favourites.call_time")}</Body>
-
-              <hr />
-
-              <p>
-                <a href={"mailto:" + t("contact.email")}>
-                  {t("contact.email")}
-                </a>
-              </p>
-
-              <Body>{t("favourites.email_disclaimer")}</Body>
-
-              <hr />
-
-              <Header2 className={contactUsTitle}>
-                {t("favourites.apply_prompt")}
-              </Header2>
-
-              <Body className={noBottomMargin}>
-                <a
-                  href={t("contact.my_vac_link")}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {t("favourites.login_link")}
-                </a>
-                &nbsp;
-                {t("favourites.login_prompt")}
-              </Body>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </div>
     );
   }
 }
