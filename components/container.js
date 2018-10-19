327dcd1b12bea4c4b84e5c417bf87c2bee529e18
Stephen McMurtry <smcmurtry@gmail.com>
2018-10-19 14:08:27 -0400

    made mobile padding more consistent with designs (#1335)

--- components/container.js
+++ components/container.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { cx, css } from "react-emotion";
+import { globalTheme } from "../theme";
 
 const style = css`
   max-width: 1170px;
   margin: 0 auto;
   padding-left: 30px;
   padding-right: 30px;
+  @media only screen and (max-width: ${globalTheme.max.xs}) {
+    padding-left: 15px;
+    padding-right: 15px;
+  }
 `;
 
 class Container extends Component {
