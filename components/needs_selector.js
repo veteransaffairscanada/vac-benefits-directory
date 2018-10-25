692d20561d705c4504ce73aca3a57bff1ae2ddf3
Steve Astels <sastels@gmail.com>
2018-10-25 09:09:00 -0400

    hide needs if organization selected

--- components/needs_selector.js
+++ components/needs_selector.js
@@ -5,41 +5,54 @@ import NeedButton from "./need_button";
 import { Grid } from "@material-ui/core";
 import { css } from "react-emotion";
 import Header4 from "./typography/header4";
-
+import { showQuestion } from "../utils/common";
 const needsButtons = css`
   display: flex;
   flex-wrap: wrap;
 `;
 
+const topBorder = css`
+  border-top: 1px solid black;
+  padding-top: 30px;
+  margin-top: 15px;
+`;
+
 export class NeedsSelector extends Component {
   render() {
     const { needs, t, store } = this.props;
-    return (
-      <div>
-        <Header4>{t("filter by category")}</Header4>
-        <Grid container spacing={16}>
-          <Grid item xs={9}>
-            <div>{t("Select all that apply")}</div>
+
+    if (showQuestion("needs", undefined, this.props.reduxState)) {
+      return (
+        <div className={topBorder}>
+          <Header4>{t("filter by category")}</Header4>
+          <Grid container spacing={16}>
+            <Grid item xs={9}>
+              <div>{t("Select all that apply")}</div>
+            </Grid>
+            <Grid item xs={12} className={needsButtons}>
+              {needs.map(need => (
+                <NeedButton key={need.id} need={need} t={t} store={store} />
+              ))}
+            </Grid>
           </Grid>
-          <Grid item xs={12} className={needsButtons}>
-            {needs.map(need => (
-              <NeedButton key={need.id} need={need} t={t} store={store} />
-            ))}
-          </Grid>
-        </Grid>
-      </div>
-    );
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
 const mapStateToProps = reduxState => {
   return {
-    needs: reduxState.needs
+    needs: reduxState.needs,
+    reduxState: reduxState
   };
 };
 
 NeedsSelector.propTypes = {
   needs: PropTypes.array.isRequired,
+  reduxState: PropTypes.object.isRequired,
   t: PropTypes.func.isRequired,
   store: PropTypes.object
 };
