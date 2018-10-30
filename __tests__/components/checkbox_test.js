dd1ec7b21d3cc9f5ad6d394e1081770511c8dbaa
smcmurtry <smcmurtry@gmail.com>
2018-10-30 16:01:37 -0400

    added checkbox test

---
+++ __tests__/components/checkbox_test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { mount } from "enzyme";
+import Checkbox from "../../components/checkbox";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
+describe("Checkbox", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      children: "label text"
+    };
+  });
+
+  it("passes axe tests", async () => {
+    let html = mount(<Checkbox {...props} />).html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+
+  it("shows children", () => {
+    expect(mount(<Checkbox {...props} />).html()).toContain("label text");
+  });
+});
