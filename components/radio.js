3c303d6183e5a7a80df8fb5a2cf75baaaa42233b
smcmurtry <smcmurtry@gmail.com>
2018-11-07 16:42:54 -0500

    wip

---
+++ components/radio.js
@@ -0,0 +1,100 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "react-emotion";
+import { globalTheme } from "../theme";
+
+const Label = styled("label")(
+  {
+    display: "block",
+    position: "relative",
+    padding: "0 0 0 38px",
+    marginBottop: "2px"
+  },
+  ({ inline }) => ({
+    float: inline ? "left" : undefined,
+    clear: inline ? "none" : undefined,
+    marginRight: inline ? "30px" : "0"
+  })
+);
+
+const Input = styled("input")(
+  {
+    position: "absolute",
+    cursor: "pointer",
+    left: 0,
+    top: 0,
+    width: "38px",
+    height: "38px",
+    zIndex: 1,
+    margin: 0,
+    zoom: 1,
+    opacity: 0,
+    ":checked + span::after": {
+      opacity: 1
+    },
+    ":focus + span::before": {
+      boxShadow: `0 0 0 3px ${globalTheme.colour.focusColour};`
+    }
+  },
+  ({ disabled }) => ({
+    cursor: disabled ? "auto" : "pointer",
+    " + span": {
+      opacity: disabled ? ".4" : "1",
+      pointerEvents: disabled ? "none" : "auto"
+    }
+  })
+);
+
+const LabelText = styled("span")({
+  fontFamily: globalTheme.fontFamily,
+  fontWeight: 400,
+  textTransform: "none",
+  fontSize: "16px",
+  lineHeight: "1.25",
+  cursor: "pointer",
+  padding: "8px 10px 9px 12px",
+  display: "block",
+  ":before": {
+    content: "''",
+    boxSizing: "border-box",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "33px",
+    height: "33px",
+    border: "2px solid black",
+    borderRadius: "50%",
+    background: "transparent"
+  },
+  ":after": {
+    content: "''",
+    position: "absolute",
+    top: "8px",
+    left: "8px",
+    width: 0,
+    height: 0,
+    border: "8.5px solid",
+    borderRadius: "50%",
+    opacity: 0
+  }
+});
+
+const Radio = ({ inline, children, className, ...input }) => (
+  <Label inline={inline} className={className}>
+    <Input type="radio" {...input} />
+    <LabelText>{children}</LabelText>
+  </Label>
+);
+
+Radio.defaultProps = {
+  inline: false,
+  className: undefined
+};
+
+Radio.propTypes = {
+  inline: PropTypes.bool,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
+export default Radio;
--- components/radio_selector.js
+++ components/radio_selector.js
@@ -1,15 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FormControl, FormControlLabel } from "@material-ui/core";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
 import { connect } from "react-redux";
 import { logEvent } from "../utils/analytics";
-import { uuidv4 } from "../utils/common";
 import { globalTheme } from "../theme";
 import { css } from "react-emotion";
 import Header from "./typography/header";
 import Tooltip from "./tooltip";
+import Radio from "./radio";
 
 const formControl = css`
   margin-top: ${globalTheme.unit} !important;
@@ -23,6 +20,19 @@ const underline = css`
 `;
 
 export class RadioSelector extends React.Component {
+  state = {
+    selected: this.props.responses[this.props.selectorType]
+  };
+
+  // componentDidMount() {
+  //   this.setState({selected: this.props.responses[this.props.selectorType]});
+  //
+  // }
+  // constructor(props) {
+  //   super(props);
+  //   this.setState({selected: props.responses[props.selectorType]});
+  // }
+
   isDisabled = (option, responses, questionDisplayLogic) => {
     let returnValue = false;
     questionDisplayLogic.forEach(row => {
@@ -73,12 +83,13 @@ export class RadioSelector extends React.Component {
     const response = event.target.value;
     logEvent("FilterClick", question, response);
     this.props.saveQuestionResponse(question, response);
+    this.setState(response);
+
     this.clearAppropriateResponses(question, response);
+    // this.forceUpdate();
   };
 
   render() {
-    const guid = uuidv4();
-
     const options = this.props.multipleChoiceOptions
       .filter(mco => this.props.options.indexOf(mco.id) !== -1)
       .filter(
@@ -91,10 +102,10 @@ export class RadioSelector extends React.Component {
       );
 
     const { t, selectorType, responses, legend, tooltipText } = this.props;
-
+    console.log(responses[selectorType]);
     if (options.length !== 0) {
       return (
-        <FormControl className={formControl}>
+        <div className={formControl}>
           <Tooltip
             disabled={!tooltipText}
             tooltipText={tooltipText}
@@ -104,30 +115,27 @@ export class RadioSelector extends React.Component {
               <span className={tooltipText ? underline : ""}>{legend}</span>
             </Header>
           </Tooltip>
-          <RadioGroup
+          <div
             aria-label={legend}
-            value={responses[selectorType]}
-            onChange={this.handleSelect}
+            // onChange={this.handleSelect}
           >
             {options.map(option => {
               return (
-                <FormControlLabel
+                <Radio
                   key={option.variable_name}
+                  name={selectorType}
+                  checked={responses[selectorType] === option.variable_name}
+                  onChange={this.handleSelect}
                   value={option.variable_name}
-                  htmlFor={option.variable_name + guid}
-                  control={
-                    <Radio color="primary" id={option.variable_name + guid} />
-                  }
-                  label={
-                    t("current-language-code") === "en"
-                      ? option.display_text_english
-                      : option.display_text_french
-                  }
-                />
+                >
+                  {t("current-language-code") === "en"
+                    ? option.display_text_english
+                    : option.display_text_french}
+                </Radio>
               );
             })}
-          </RadioGroup>
-        </FormControl>
+          </div>
+        </div>
       );
     } else {
       return null;
@@ -153,6 +161,7 @@ const mapStateToProps = reduxState => {
     questionDisplayLogic: reduxState.questionDisplayLogic,
     questionClearLogic: reduxState.questionClearLogic,
     responses: reduxState
+    // answer: reduxState[this.props.selectorType]
   };
 };
 
@@ -168,6 +177,7 @@ RadioSelector.propTypes = {
   questionClearLogic: PropTypes.array.isRequired,
   options: PropTypes.array.isRequired,
   tooltipText: PropTypes.string,
+  // answer: PropTypes.string,
   store: PropTypes.object
 };
 
