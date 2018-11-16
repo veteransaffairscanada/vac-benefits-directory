9ef4dfbae3ced262f4ee55c730d1bc8e14f10f70
smcmurtry <smcmurtry@gmail.com>
2018-11-16 16:05:08 -0500

    added radio tests

---
+++ __tests__/components/radio_test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { mount } from "enzyme";
+import Radio from "../../components/radio";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
+describe("Radio", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      children: "label text",
+      value: "value"
+    };
+  });
+
+  it("passes axe tests", async () => {
+    let html = mount(<Radio {...props} />).html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+
+  it("shows children", () => {
+    expect(mount(<Radio {...props} />).html()).toContain("label text");
+  });
+});
