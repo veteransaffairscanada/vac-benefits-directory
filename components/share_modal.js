a6f17aee6af512b2c7709d8cd45ace9817db640f
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2018-11-21 16:06:39 -0500

    First round of styling on share_modal

--- components/share_modal.js
+++ components/share_modal.js
@@ -1,20 +1,91 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { css } from "react-emotion";
+import styled from "react-emotion";
 import ReactModal from "react-modal";
+
+import { globalTheme } from "../theme";
 
 const modalCSS = css`
   position: absolute;
   top: 20%;
-  left: 40px;
-  right: 40px;
-  border: 1px solid rgb(204, 204, 204);
+  left: 100px;
+  right: 100px;
+  border: 0;
   background: rgb(255, 255, 255);
   overflow: auto;
   border-radius: 4px;
   outline: none;
-  padding: 0 2em 2em 2em;
+  padding: 0;
 `;
+
+const header = css`
+  background-color: ${globalTheme.colour.darkGreyBlue};
+  color: ${globalTheme.colour.white};
+  padding: 0.75em 1.5em;
+  font-size: 18px;
+`;
+
+const bodyStyle = css`
+  padding: 1.5em;
+`;
+
+const URLInputBox = styled("input")({
+  width: "80%",
+  height: "44px",
+  padding: "9px 19px 8px 19px",
+  margin: 0,
+  border: 0,
+  boxShadow:
+    "0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)",
+  boxSizing: "border-box",
+  fontFamily: globalTheme.fontFamily,
+  fontWeight: 400,
+  textTransform: "none",
+  fontSize: "14px",
+  lineHeight: "1.5",
+  background: globalTheme.colour.white,
+  borderRadius: 0,
+  WebkitAppearance: "none",
+  ":focus": {
+    marginRight: "3px",
+    outline: `3px solid ` + globalTheme.colour.focusColour,
+    outlineOffset: 0
+  }
+});
+
+const CopyButton = styled("button")({
+  backgroundColor: globalTheme.colour.cerulean,
+  cursor: "pointer",
+  border: 0,
+  borderRadius: "3px",
+  color: globalTheme.colour.white,
+  fontFamily: globalTheme.fontFamily,
+  fontWeight: "bold",
+  fontSize: "18px",
+  lineHeight: "23px",
+  position: "relative",
+  padding: ".526315em 1em",
+  marginLeft: "1em",
+  backgroundPosition: "2px 50%",
+  ":focus": {
+    outlineOffset: 0,
+    outline: `3px solid ` + globalTheme.colour.focusColour
+  },
+  ":hover": {
+    backgroundColor: globalTheme.colour.darkGreyBlue
+  }
+});
+
+const CloseButton = styled("button")({
+  float: "right",
+  backgroundColor: globalTheme.colour.darkGreyBlue,
+  height: "100%",
+  cursor: "pointer",
+  fontSize: "18px",
+  border: 0,
+  color: globalTheme.colour.white
+});
 
 class ShareModal extends Component {
   constructor(props) {
@@ -52,15 +123,22 @@ class ShareModal extends Component {
         isOpen={isOpen}
         onRequestClose={onRequestClose}
       >
-        <div>
-          <p>Share this page</p>
-          <button onClick={closeModal}>X</button>
+        <div className={header}>
+          <span>Share this page</span>
+          <CloseButton onClick={closeModal}>X</CloseButton>
         </div>
-        <p>Copy the link below and paste it wherever you need.</p>
-        <input type="text" id="shareTarget" value={this.state.url} readOnly />
-        <button data-copytarget="#shareTarget" onClick={this.copyText}>
-          Copy
-        </button>
+        <div className={bodyStyle}>
+          <p>Copy the link below and paste it wherever you need.</p>
+          <URLInputBox
+            type="text"
+            id="shareTarget"
+            value={this.state.url}
+            readOnly
+          />
+          <CopyButton data-copytarget="#shareTarget" onClick={this.copyText}>
+            Copy
+          </CopyButton>
+        </div>
       </ReactModal>
     );
   }
@@ -74,5 +152,6 @@ ShareModal.propTypes = {
 };
 
 ReactModal.setAppElement("#__next");
+ReactModal.defaultStyles.overlay.backgroundColor = "rgba(30,30,30,0.75)";
 
 export default ShareModal;
