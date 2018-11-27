99eaa981a81c14247b566fec2014e5c485c0d23d
SupeDeDupe <sethompson@upei.ca>
2018-11-27 14:40:06 -0400

    added a summary page for ge

---
+++ pages/guided_summary.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import withI18N from "../lib/i18nHOC";
+import Layout from "../components/layout";
+import PropTypes from "prop-types";
+import GuidedExperience from "../components/guided_experience";
+
+export class GuidedSummary extends Component {
+  render() {
+    const { t, i18n } = this.props;
+
+    return (
+      <Layout
+        i18n={i18n}
+        t={t}
+        hideNoscript={false}
+        showRefreshCache={false}
+        title={t("titles.ge_summary")}
+      >
+        <GuidedExperience
+          id={"summary"}
+          stepNumber={6}
+          nextSection={"benefits-directory"}
+          /*prevSection={this.getPrevSection(
+                        displayable_sections,
+                        dynamicStepNumber
+                    )}*/
+          setSection={this.setSection}
+          subtitle={t("ge.summary_subtitle")}
+          helperText={t("ge.summary_tooltip")}
+          t={t}
+        />
+      </Layout>
+    );
+  }
+}
+
+GuidedSummary.propTypes = {
+  i18n: PropTypes.object.isRequired,
+  t: PropTypes.func.isRequired
+};
+
+export default withI18N(GuidedSummary);
