391c1b3fdcdb890019d9391091f7466150fdae51
SupeDeDupe <sethompson@upei.ca>
2018-12-03 12:07:43 -0400

    fixed layout colour in b-directory

--- components/BB.js
+++ components/BB.js
@@ -23,7 +23,6 @@ const outerDiv = css`
 `;
 const topPadding = css`
   padding-top: 30px;
-  background-color: ${globalTheme.colour.white};
 `;
 const topMatter = css`
   background-color: ${globalTheme.colour.white};
