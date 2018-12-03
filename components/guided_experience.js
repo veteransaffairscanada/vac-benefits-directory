8ce112ba654390bdc95f0607e825919a5253bea4
smcmurtry <smcmurtry@gmail.com>
2018-12-03 13:14:25 -0400

    removed GE breadcrumbs

--- components/guided_experience.js
+++ components/guided_experience.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Grid } from "@material-ui/core";
-import { connect } from "react-redux";
 import { css } from "react-emotion";
 import Router from "next/router";
 import Container from "./container";
-import FilterText from "./typography/filter_text";
 import Header from "./typography/header";
 import Body from "./typography/body";
 import Button from "./button";
 import HeaderButton from "./header_button";
 import HeaderLink from "./header_link";
-import AnchorLink from "./typography/anchor_link";
 import { globalTheme } from "../theme";
 import Paper from "./paper";
 import { mutateUrl } from "../utils/common";
@@ -27,9 +24,6 @@ const prevButton = css`
   margin-top: 50px;
   margin-bottom: 15px;
 `;
-const comma = css`
-  margin-right: 0.5em;
-`;
 const questions = css`
   margin: 0;
   padding: 0;
@@ -39,49 +33,9 @@ const body = css`
   margin-bottom: 0px;
 `;
 export class GuidedExperience extends Component {
-  jumpButtons = (t, reduxState) => {
-    const eligibilityKeys = reduxState.questions
-      .map(x => x.variable_name)
-      .filter(x => x != "needs");
-    let jsx_array = eligibilityKeys.map((k, i) => {
-      if (!reduxState[k] || k === this.props.id) {
-        return null;
-      } else {
-        let option = reduxState.multipleChoiceOptions.filter(
-          x => x.variable_name === reduxState[k]
-        )[0];
-        let text;
-        if (t("current-language-code") === "en") {
-          text = option.ge_breadcrumb_english
-            ? option.ge_breadcrumb_english
-            : option.display_text_english;
-        } else {
-          text = option.ge_breadcrumb_french
-            ? option.ge_breadcrumb_french
-            : option.display_text_french;
-        }
-
-        return (
-          <span key={i}>
-            <span className={comma}>{i === 0 ? "" : ","}</span>
-            <AnchorLink
-              id={"jumpButton" + i}
-              href="#"
-              onClick={() => this.props.setSection(k)}
-            >
-              {text}
-            </AnchorLink>
-          </span>
-        );
-      }
-    });
-    return jsx_array;
-  };
-
   render() {
     const {
       t,
-      reduxState,
       prevSection,
       nextSection,
       subtitle,
@@ -89,9 +43,6 @@ export class GuidedExperience extends Component {
       helperText,
       url
     } = this.props;
-
-    const jumpButtons = this.jumpButtons(t, reduxState);
-    const nonNullBreadcrumbs = jumpButtons.filter(x => x != null);
 
     return (
       <Container id="guidedExperience">
@@ -131,15 +82,6 @@ export class GuidedExperience extends Component {
 
         <Paper padding="md" className={box}>
           <Grid container spacing={24}>
-            <Grid item xs={12} md={12}>
-              <FilterText style={{ display: "inline-block" }}>
-                {nonNullBreadcrumbs.length === 0
-                  ? ""
-                  : t("B3.Filter by eligibility")}
-              </FilterText>
-              {jumpButtons}
-            </Grid>
-
             <Grid item xs={12} className={questions}>
               <Header size="md_lg" headingLevel="h2">
                 {subtitle}
@@ -181,12 +123,6 @@ export class GuidedExperience extends Component {
   }
 }
 
-const mapStateToProps = reduxState => {
-  return {
-    reduxState: reduxState
-  };
-};
-
 GuidedExperience.propTypes = {
   id: PropTypes.string.isRequired,
   url: PropTypes.object.isRequired,
@@ -198,8 +134,7 @@ GuidedExperience.propTypes = {
   helperText: PropTypes.string,
   stepNumber: PropTypes.number.isRequired,
   children: PropTypes.object.isRequired,
-  reduxState: PropTypes.object.isRequired,
   store: PropTypes.object
 };
 
-export default connect(mapStateToProps)(GuidedExperience);
+export default GuidedExperience;
