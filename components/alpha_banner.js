52f2eadbb78f4c3b4eb6eff53ab289fec9750ee2
Steve Astels <sastels@gmail.com>
2018-12-10 15:16:52 -0500

    change first-child

--- components/alpha_banner.js
+++ components/alpha_banner.js
@@ -14,7 +14,7 @@ const Banner = css`
   min-width: 20em;
   color: ${globalTheme.colour.white};
   font: 0.694rem sans-serif;
-  span:first-child {
+  span:first-of-type {
     font-weight: 700 !important;
     padding: 0.2rem 0.7rem;
     border-radius: 5px;
