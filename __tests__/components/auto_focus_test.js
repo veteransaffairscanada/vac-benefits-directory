fd1106d411950cc3ca4907c86875c24ef90d1f26
smcmurtry <smcmurtry@gmail.com>
2018-12-21 11:56:00 -0500

    added tests

--- __tests__/components/auto_focus_test.js
+++ __tests__/components/auto_focus_test.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { mount } from "enzyme";
 import AutoFocus from "../../components/auto_focus";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
 
-describe("<WordMark />", () => {
-  it("renders svg", () => {
-    let wrapper = mount(<AutoFocus />);
-    expect(wrapper.is("svg")).toBeTruthy();
+describe("AutoFocus", () => {
+  let props;
+
+  beforeEach(() => {
+    window.scrollTo = jest.fn();
+    props = {
+      children: <div id="test">blah</div>
+    };
+  });
+
+  it("passes axe tests", async () => {
+    let html = mount(<AutoFocus {...props} />).html();
+    expect(await axe(html)).toHaveNoViolations();
+  });
+
+  it("renders children", async () => {
+    expect(mount(<AutoFocus {...props} />).find("#test").length).toEqual(1);
   });
 });
