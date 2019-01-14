ab95c1284cfaaa28f3eddc87c5ae87fbf0e51b7e
Stephen McMurtry <smcmurtry@gmail.com>
2019-01-14 15:41:08 -0500

    made skip to main content button its own component, fixed language button bug

--- components/federal_banner.js
+++ components/federal_banner.js
@@ -5,6 +5,7 @@ import { Button } from "@material-ui/core";
 import LanguageButton from "./language_button";
 import FIP from "./fip";
 import { globalTheme } from "../theme";
+import SkipToMainContent from "./skip_to_main_content";
 
 export const breakpoints = {
   xs: 481,
@@ -67,45 +68,12 @@ const container = css`
   `)};
 `;
 
-const skipLinkContainer = css`
-  position: absolute;
-  text-align: center;
-  width: 90%;
-  z-index: 3;
-  top: 10px;
-  left: 0px;
-  list-style: none;
-`;
-
-const skipLinkStyle = css`
-  width: 1px !important;
-  height: 1px !important;
-  overflow: hidden !important;
-  display: inline-block;
-  color: white;
-  text-decoration: none;
-  font-family: ${globalTheme.fontFamily};
-
-  :focus {
-    width: auto !important;
-    height: auto !important;
-    outline: 3px solid #ffbf47;
-    outline-offset: 0;
-  }
-`;
-
 class FederalBanner extends Component {
   render() {
     const { t, skipLink } = this.props;
     return (
       <div>
-        <ul className={skipLinkContainer}>
-          <li>
-            <a className={skipLinkStyle} href={skipLink} id="skipLink">
-              {t("skipLink")}
-            </a>
-          </li>
-        </ul>
+        <SkipToMainContent skipLink={skipLink} t={t} />
         <div className={container}>
           <div className="svg-container">
             <FIP fillColor="white" t={this.props.t} />
