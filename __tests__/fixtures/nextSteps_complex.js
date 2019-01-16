11a80185f02f6a7bcf0625f247bb48df684c033d
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2019-01-16 14:05:22 -0500

    update fixture data for tests

--- __tests__/fixtures/nextSteps_complex.js
+++ __tests__/fixtures/nextSteps_complex.js
@@ -5,8 +5,8 @@ const nextSteps = [
       "Have you scheduled your [Transition Interview](http://www.veterans.gc.ca/eng/services/transition/interview) yet? Every currently serving member is eligible to talk to a VAC person to make transitioning out of the military as seamless as possible. ",
     french:
       "Avez-vous planifi\u00e9 votre [entrevue de transition](http://www.veterans.gc.ca/fra/services/transition/interview)? Tout membre actif a droit \u00e0 un entretien avec un agent d\u2019ACC afin de s\u2019assurer que la transition de la vie militaire \u00e0 la vie civile se passe le mieux possible.",
-    eligibilityPath: ["recR03cJel83BGYSD"],
-    id: "recKyNwxKXQQGcokF"
+    id: "recKyNwxKXQQGcokF",
+    patronType: "servingMember"
   },
   {
     bullet_name: "doctorsNote",
@@ -15,7 +15,9 @@ const nextSteps = [
     french:
       "Assurez-vous d\u2019avoir en main un billet du m\u00e9decin attestant de votre probl\u00e8me de sant\u00e9 reli\u00e9 au service. Votre processus de demande d\u2019avantages en sera grandement facilit\u00e9.",
     eligibilityPath: ["recBlSOo6diB5tkwc"],
-    id: "recp2EOCHfUhGU2jq"
+    id: "recp2EOCHfUhGU2jq",
+    patronType: "family",
+    serviceType: "CAF"
   },
   {
     bullet_name: "scanSession",
@@ -24,7 +26,8 @@ const nextSteps = [
     french:
       "Assistez \u00e0 une s\u00e9ance du SPSC pour en apprendre davantage sur ce que l\u2019ACC peut faire pour vous.",
     eligibilityPath: ["recR03cJel83BGYSD"],
-    id: "recdLLujXxdXQEzXC"
+    id: "recdLLujXxdXQEzXC",
+    patronType: "servingMember"
   },
   {
     bullet_name: "firstStep",
