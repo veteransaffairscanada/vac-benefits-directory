30abe43274afb3f7c2d6fce0d9e61392de6d44a0
Sarah Thompson <sethompson@upei.ca>
2019-01-17 15:42:19 -0400

    Analytics fix (#1717)
    * fixed the analytics for configuring multiple trackers
    
    * changed the implementation to better suit tests
    
    * empty

--- utils/analytics.js
+++ utils/analytics.js
@@ -5,30 +5,36 @@ export const initGA = () => {
     [
       {
         trackingId: process.env.GA_UA,
-        gaOptions: { name: "tracker_1" }
+        gaOptions: { name: "tracker_1", siteSpeedSampleRate: 100 } // This will send 100% of hits to Google Analytics. By default only 1% are sent.
       },
       {
-        trackingId: process.env.GA_UA_CDS
+        trackingId: process.env.GA_UA_CDS,
+        gaOptions: { name: "tracker_2", siteSpeedSampleRate: 100 }
       }
     ]
-    // , { debug: true }
+    //, { debug: true }
   );
-  ReactGA.set({ anonymizeIp: true });
+  ReactGA.set({ anonymizeIp: true }, ["tracker_1"]);
+  ReactGA.set({ anonymizeIp: true }, ["tracker_2"]);
 };
 
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  ReactGA.set({ page: window.location.pathname }, ["tracker_1"]);
+  ReactGA.pageview(window.location.pathname, ["tracker_1"]);
+  ReactGA.set({ page: window.location.pathname }, ["tracker_2"]);
+  ReactGA.pageview(window.location.pathname, ["tracker_2"]);
 };
 
 export const logEvent = (category = "", action = "", label = "") => {
   if (category && action) {
-    ReactGA.event({ category, action, label });
+    ReactGA.event({ category, action, label }, ["tracker_1"]);
+    ReactGA.event({ category, action, label }, ["tracker_2"]);
   }
 };
 
 export const logException = (description = "", fatal = false) => {
   if (description) {
-    ReactGA.exception({ description, fatal });
+    ReactGA.exception({ description, fatal }, ["tracker_1"]);
+    ReactGA.exception({ description, fatal }, ["tracker_2"]);
   }
 };
