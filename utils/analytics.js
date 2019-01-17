0e13f8e0cc501f5b6a8215eb28b6846aa7352b4b
SupeDeDupe <sethompson@upei.ca>
2019-01-17 10:44:26 -0400

    changed the implementation to better suit tests

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,40 +1,40 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => {
-  console.log("GA_UA", process.env.GA_UA);
-  console.log("GA_UA_CDS", process.env.GA_UA_CDS);
-
   ReactGA.initialize(
     [
       {
         trackingId: process.env.GA_UA,
-        gaOptions: { name: "tracker_1", siteSpeedSampleRate: 100 }
+        gaOptions: { name: "tracker_1", siteSpeedSampleRate: 100 } // This will send 100% of hits to Google Analytics. By default only 1% are sent.
       },
       {
         trackingId: process.env.GA_UA_CDS,
         gaOptions: { name: "tracker_2", siteSpeedSampleRate: 100 }
       }
-    ],
-    { debug: true }
+    ]
+    //, { debug: true }
   );
-  ReactGA.set({ anonymizeIp: true });
+  ReactGA.set({ anonymizeIp: true }, ["tracker_1"]);
+  ReactGA.set({ anonymizeIp: true }, ["tracker_2"]);
 };
 
 export const logPageView = () => {
-  ga("tracker_1.set", "page", window.location.pathname);
-  ga("tracker_1.send", "pageview", window.location.pathname);
-  ga("tracker_2.set", "page", window.location.pathname);
-  ga("tracker_2.send", "pageview", window.location.pathname);
+  ReactGA.set({ page: window.location.pathname }, ["tracker_1"]);
+  ReactGA.pageview(window.location.pathname, ["tracker_1"]);
+  ReactGA.set({ page: window.location.pathname }, ["tracker_2"]);
+  ReactGA.pageview(window.location.pathname, ["tracker_2"]);
 };
 
 export const logEvent = (category = "", action = "", label = "") => {
   if (category && action) {
-    ReactGA.event({ category, action, label });
+    ReactGA.event({ category, action, label }, ["tracker_1"]);
+    ReactGA.event({ category, action, label }, ["tracker_2"]);
   }
 };
 
 export const logException = (description = "", fatal = false) => {
   if (description) {
-    ReactGA.exception({ description, fatal });
+    ReactGA.exception({ description, fatal }, ["tracker_1"]);
+    ReactGA.exception({ description, fatal }, ["tracker_2"]);
   }
 };
