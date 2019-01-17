712a6a1e73af3cafe027143a07779dac631dceb4
SupeDeDupe <sethompson@upei.ca>
2019-01-17 10:06:33 -0400

    fixed the analytics for configuring multiple trackers

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,24 +1,30 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => {
+  console.log("GA_UA", process.env.GA_UA);
+  console.log("GA_UA_CDS", process.env.GA_UA_CDS);
+
   ReactGA.initialize(
     [
       {
         trackingId: process.env.GA_UA,
-        gaOptions: { name: "tracker_1" }
+        gaOptions: { name: "tracker_1", siteSpeedSampleRate: 100 }
       },
       {
-        trackingId: process.env.GA_UA_CDS
+        trackingId: process.env.GA_UA_CDS,
+        gaOptions: { name: "tracker_2", siteSpeedSampleRate: 100 }
       }
-    ]
-    // , { debug: true }
+    ],
+    { debug: true }
   );
   ReactGA.set({ anonymizeIp: true });
 };
 
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  ga("tracker_1.set", "page", window.location.pathname);
+  ga("tracker_1.send", "pageview", window.location.pathname);
+  ga("tracker_2.set", "page", window.location.pathname);
+  ga("tracker_2.send", "pageview", window.location.pathname);
 };
 
 export const logEvent = (category = "", action = "", label = "") => {
