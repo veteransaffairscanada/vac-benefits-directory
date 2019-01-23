d59b649e0588a35b5c7d765fa4a4baddd63581f4
smcmurtry <smcmurtry@gmail.com>
2019-01-23 17:24:00 -0500

    more refactoring

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,32 @@ import Cookies from "universal-cookie";
 import { connect } from "react-redux";
 import withI18N from "../lib/i18nHOC";
 import Layout from "../components/layout";
-import GuidedExperience from "../components/guided_experience";
 import GuidedExperienceProfile from "../components/guided_experience_profile";
+import { Grid } from "@material-ui/core";
+import { css } from "emotion";
+import { globalTheme } from "../theme";
+import Paper from "../components/paper";
+import Header from "../components/typography/header";
+import Body from "../components/typography/body";
+import NextSkipButtons from "../components/next_skip_buttons";
+import Container from "../components/container";
+import HeaderLink from "../components/header_link";
+
+const box = css`
+  padding: 63px 63px 63px 63px;
+  @media only screen and (max-width: ${globalTheme.max.mobile}) {
+    padding: 17px 26px 55px 26px;
+  }
+  display: inline-flex;
+`;
+const prevButton = css`
+  margin-top: 50px;
+  margin-bottom: 15px;
+`;
+const questions = css`
+  margin: 0;
+  padding: 0;
+`;
 
 const section = "patronType";
 
@@ -35,10 +59,12 @@ export class Index extends Component {
   };
 
   render() {
-    const { t, i18n, store, reduxState, sectionOrder, url } = this.props;
+    const { t, i18n, store, reduxState, url } = this.props;
     const question = reduxState.questions.filter(
       x => x.variable_name === section
     )[0];
+    const id = section;
+    const subtitle = this.getSubtitle(question);
 
     return (
       <Layout
@@ -48,22 +74,42 @@ export class Index extends Component {
         title={t("ge.Find benefits and services")}
         skipLink="#mainContent"
       >
-        <GuidedExperience
-          id={section}
-          stepNumber={sectionOrder.indexOf(section)}
-          prevSection="index"
-          subtitle={this.getSubtitle(question)}
-          t={t}
-          url={url}
-          store={store}
-        >
-          <GuidedExperienceProfile
-            t={t}
-            selectorType={section}
-            store={store}
-            options={question["multiple_choice_options"]}
-          />
-        </GuidedExperience>
+        <Container id="mainContent">
+          <HeaderLink
+            id="prevButton"
+            href={t("ge.home_link")}
+            className={prevButton}
+            arrow="back"
+          >
+            {t("back")}
+          </HeaderLink>
+          <Header size="lg" headingLevel="h1">
+            {t("ge.Find benefits and services")}
+          </Header>
+          <Body>
+            <p>{t("ge.intro_text_p1")}</p>
+            <p>{t("ge.intro_text_p2")}</p>
+          </Body>
+
+          <Paper padding="md" className={box}>
+            <Grid container spacing={24}>
+              <Grid item xs={12} className={questions}>
+                <Header size="md_lg" headingLevel="h2">
+                  {subtitle}
+                </Header>
+                <GuidedExperienceProfile
+                  t={t}
+                  selectorType={section}
+                  store={store}
+                  options={question["multiple_choice_options"]}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <NextSkipButtons t={t} id={id} url={url} />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Container>
       </Layout>
     );
   }
