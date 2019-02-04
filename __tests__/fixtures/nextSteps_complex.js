82f623a54a515cb66f3b32c59ea25a018a64b602
Sarah Thompson <sethompson@upei.ca>
2019-02-04 15:46:22 -0500

    Fixing nextStepsFixture and next steps tests in benefits_test  (#1795)
    * changed test expects
    
    * fixed tests
    
    * removed console log

--- __tests__/fixtures/nextSteps_complex.js
+++ __tests__/fixtures/nextSteps_complex.js
@@ -6,7 +6,7 @@ const nextSteps = [
     french:
       "Avez-vous planifi\u00e9 votre [entrevue de transition](http://www.veterans.gc.ca/fra/services/transition/interview)? Tout membre actif a droit \u00e0 un entretien avec un agent d\u2019ACC afin de s\u2019assurer que la transition de la vie militaire \u00e0 la vie civile se passe le mieux possible.",
     id: "recKyNwxKXQQGcokF",
-    patronType: "servingMember"
+    patronType: ["recC9OodJNCqbnGy2"]
   },
   {
     bullet_name: "doctorsNote",
@@ -14,9 +14,8 @@ const nextSteps = [
       "Make sure you have a doctor\u2019s note for your service-related health issue. This will make applying for these benefits go a lot smoother.",
     french:
       "Assurez-vous d\u2019avoir en main un billet du m\u00e9decin attestant de votre probl\u00e8me de sant\u00e9 reli\u00e9 au service. Votre processus de demande d\u2019avantages en sera grandement facilit\u00e9.",
-    eligibilityPath: ["recBlSOo6diB5tkwc"],
     id: "recp2EOCHfUhGU2jq",
-    patronType: "family",
+    patronType: ["recuWkVDSEWc1K0eU"],
     serviceType: "CAF"
   },
   {
@@ -25,16 +24,14 @@ const nextSteps = [
       "Attend a SCAN session to learn more about what VAC can do for you.",
     french:
       "Assistez \u00e0 une s\u00e9ance du SPSC pour en apprendre davantage sur ce que l\u2019ACC peut faire pour vous.",
-    eligibilityPath: ["recR03cJel83BGYSD"],
     id: "recdLLujXxdXQEzXC",
-    patronType: "servingMember"
+    patronType: ["recC9OodJNCqbnGy2"]
   },
   {
     bullet_name: "firstStep",
     english:
       "If you are looking for more information please look at _See more_.",
     french: "(fra)If you are looking for more information look at See more.",
-    eligibilityPath: ["recDABExyenzg5QNw"],
     id: "recDGuWEo84Pie18s"
   },
   {
@@ -43,14 +40,12 @@ const nextSteps = [
       "If you wish to complete an application please see _Register for My VAC Account_.",
     french:
       "(fra)If you wish to complete an application please see _Register for My VAC Account_",
-    eligibilityPath: ["recDABExyenzg5QNw"],
     id: "recg5X7mEmlm4Qb2C"
   },
   {
     bullet_name: "thirdStep",
     english: "If you saved a card to your list please look at _Saved list_.",
     french: "(fra)If you saved a card to your list please look at Saved list.",
-    eligibilityPath: ["recDABExyenzg5QNw"],
     id: "recyxzgPs14QzD8uS"
   }
 ];
