4cf94b4179ebfb2f33d7eaa480f860184f830197
Steve Astels <sastels@gmail.com>
2019-02-06 10:32:39 -0500

    make yarn download command

--- scripts/download.js
+++ scripts/download.js
@@ -1,9 +1,9 @@
-const airTable = require("./airtable_es2015");
+const airTable = require("../utils/airtable_es2015");
+const fs = require("fs");
 
 Promise.resolve(airTable.hydrateFromAirtable()).then(data => {
   const json = JSON.stringify(data, null, 4);
-  const fileName = "data.json";
-  var fs = require("fs");
+  const fileName = process.argv[2];
   fs.writeFile(fileName, json, err => {
     console.log(err || `data written to ${fileName}`);
   });
