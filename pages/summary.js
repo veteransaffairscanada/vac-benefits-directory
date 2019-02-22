a2e671ffa2a4c6af39badf8464033b37819cb7e4
SupeDeDupe <sethompson@upei.ca>
2019-02-22 13:35:38 -0400

    changed background colour

--- pages/summary.js
+++ pages/summary.js
@@ -55,6 +55,7 @@ export class Summary extends Component {
         t={t}
         hideNoscript={false}
         title={t("titles.ge_summary")}
+        backgroundColor={globalTheme.colour.paleGreyTwo}
         skipLink="mainContent"
         url={url}
       >
