a6e2ab9b44da1a74073c6b48d50ee72eafa8eb98
SupeDeDupe <sethompson@upei.ca>
2019-02-26 15:48:52 -0400

    added markdown parsing to benefit alerts

--- components/card_header_important_info.js
+++ components/card_header_important_info.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import MarkdownIt from "markdown-it";
+import JsxParser from "react-jsx-parser";
 
 export class CardHeaderImportantInfo extends Component {
+  md = new MarkdownIt();
   benefitNote = benefit => {
-    return this.props.t("current-language-code") === "en"
-      ? benefit.noteEn
-      : benefit.noteFr;
+    const note =
+      this.props.t("current-language-code") === "en"
+        ? benefit.noteEn
+        : benefit.noteFr;
+
+    let jsxString = this.md
+      .render(note)
+      .replace("<p>", "<span>")
+      .replace("</p>", "</span>");
+    return <JsxParser jsx={jsxString} />;
   };
 
   render() {
