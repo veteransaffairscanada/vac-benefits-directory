772d5e4f3b9ba113df3e0fd6aa62f472ce6a7455
smcmurtry <smcmurtry@gmail.com>
2019-03-01 11:17:10 -0500

    wip

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,7 @@ import Noscript from "../components/noscript";
 import Container from "../components/container";
 import { globalTheme } from "../theme";
 import Link from "next/link";
+import VacFooter from "./vac_footer";
 
 const alpha = css`
   background-color: ${globalTheme.colour.alphaBlue};
@@ -69,6 +70,27 @@ class Layout extends Component {
   render() {
     const { t, title, skipLink, url } = this.props;
     const noScriptTag = this.props.hideNoscript ? null : <Noscript t={t} />;
+
+    // fetch('https://veterans.gc.ca/eng')
+    //     .then((res) => {
+    //       console.log(res)
+    //         // return res.json();
+    //     })
+
+    var proxyUrl = "https://cors-anywhere.herokuapp.com/",
+      targetUrl = "https://veterans.gc.ca/eng#wb-info";
+    fetch(proxyUrl + targetUrl)
+      .then(blob => blob.text())
+      .then(data => {
+        // console.log(data.text());
+        document.querySelector("pre").innerHTML = data; //JSON.stringify(data, null, 2);
+        return data;
+      })
+      .catch(e => {
+        console.log(e);
+        return e;
+      });
+
     return (
       <MuiThemeProvider theme={theme}>
         <div style={{ backgroundColor: this.props.backgroundColor }}>
@@ -106,6 +128,7 @@ class Layout extends Component {
             <div className={backgoundColour2}>
               <Container>
                 <Footer t={t} />
+                <pre />
               </Container>
             </div>
           </ErrorBoundary>
