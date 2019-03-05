624a61fe8bd6774b6570218b356a9d2b0c4643a1
SupeDeDupe <sethompson@upei.ca>
2019-03-05 11:45:20 -0400

    changed font colors on top of ben dir pg

--- components/quick_links.js
+++ components/quick_links.js
@@ -22,7 +22,7 @@ const leftDiv = css`
 const rightDiv = css`
   font-family: ${globalTheme.fontFamilySerif};
   font-size: 24px;
-  color: ${globalTheme.colour.slateGrey};
+  color: ${globalTheme.colour.navy};
   border-left: 4px solid ${globalTheme.colour.duckEggBlue};
   height: 100%;
   box-sizing: border-box;
@@ -41,12 +41,13 @@ const link = css`
 const quickLinksText = css`
   font-family: ${globalTheme.fontFamilySansSerif};
   font-size: 12px;
-  color: ${globalTheme.colour.blueGrey};
+  color: ${globalTheme.colour.navy};
   font-weight: bold;
   text-transform: uppercase;
 `;
 
 const linkStyle = css`
+  color: ${globalTheme.colour.navy};
   text-align: left;
   font-size: 16px;
   font-weight: 400;
