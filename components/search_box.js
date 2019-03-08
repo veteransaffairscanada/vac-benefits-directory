ddfe95d88db4743e0a6a1b2865a843a8e39a6df1
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2019-03-08 08:58:17 -0500

    Add search role

--- components/search_box.js
+++ components/search_box.js
@@ -130,6 +130,7 @@ class SearchBox extends Component {
         <InputSearchBox
           type="search"
           aria-label={ariaLabel}
+          aria-role="search"
           id={this.props.inputId}
           placeholder={this.props.placeholder}
           onKeyDown={this.props.onKeyDown}
