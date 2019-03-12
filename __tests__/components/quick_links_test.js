5bf8b438bc98d4c09dd64aa03deacc4eefd33bfb
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2019-03-12 12:36:01 -0400

    improve coverage of quick_links

--- __tests__/components/quick_links_test.js
+++ __tests__/components/quick_links_test.js
@@ -23,4 +23,12 @@ describe("QuickLinks", () => {
     mounted.find("#next-steps-button").simulate("click");
     expect(mounted.instance().scrollToId).toBeCalledWith("#next-steps");
   });
+  it("clicking benefits and services changes window location", () => {
+    let mounted = mount(<QuickLinks {...props} />);
+    mounted.instance().scrollToId = jest.fn();
+    mounted.find("#benefits-and-services-button").simulate("click");
+    expect(mounted.instance().scrollToId).toBeCalledWith(
+      "#benefits-and-services"
+    );
+  });
 });
