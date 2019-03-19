f82960ab0b0b1debbfea9f5975d56933154167b8
Thomas Craig <thomas.craig@vac-acc.gc.ca>
2019-03-19 13:37:40 -0300

    update deploy script

--- .circleci/notify_failure.sh
+++ .circleci/notify_failure.sh
@@ -1,3 +1,7 @@
+if [ "${CIRCLE_BRANCH}" == "develop" ]; then
+    curl -X POST --data-urlencode "payload={\"text\": \"<!here> Develop is failing!!!\"}" $VAC_DEV_SLACK_URL
+fi
+
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
     curl -X POST --data-urlencode "payload={\"text\": \"<!here> Master is failing!!!\"}" $VAC_DEV_SLACK_URL
 fi
