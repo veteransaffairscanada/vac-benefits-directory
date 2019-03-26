7b2bcc45d5778979a76895db71d1d5ec7202e385
Thomas Craig <thomas.craig@vac-acc.gc.ca>
2019-03-26 07:57:55 -0300

    disabled eslint console rule for dev

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export class Index extends Component {
       ? process.env.CIRCLE_SHA1.substring(0, 7)
       : process.env.NODE_ENV;
 
+    // eslint-disable-next-line no-console
     console.log(envDetails);  // temporary to be removed before release
 
     return <GuidedExperiencePage section="patronType" {...this.props} />;
