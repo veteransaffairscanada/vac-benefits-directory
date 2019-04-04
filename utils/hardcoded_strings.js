f7995e4501d4e53248e55803b9c201c03f868489
SupeDeDupe <sethompson@upei.ca>
2019-04-04 16:48:59 -0300

    added tables back

--- utils/hardcoded_strings.js
+++ utils/hardcoded_strings.js
@@ -1,4 +1,15 @@
-exports.tableNames = ["translations"];
+exports.tableNames = [
+  "benefits",
+  "benefitEligibility",
+  "needs",
+  "translations",
+  "questions",
+  "multipleChoiceOptions",
+  "questionDisplayLogic",
+  "questionClearLogic",
+  "benefitExamples",
+  "nextSteps"
+];
 
 exports.servicePersonOptions = ["veteran", "servingMember"];
 exports.familyOptions = ["family"];
