35da642afad7a9f79f3e4a8000843a89873c94c0
Thomas Craig <thomas.craig@vac-acc.gc.ca>
2019-05-02 15:20:47 -0300

    test

---
+++ .circleci/get-semvar.sh
@@ -0,0 +1,25 @@
+branch=$(git rev-parse --abbrev-ref HEAD)
+latest=$(git describe --tags $(git rev-list --tags --max-count=1))
+ 
+semver_parts=(${latest//./ })
+major=${semver_parts[0]}
+minor=${semver_parts[1]}
+patch=${semver_parts[2]}
+ 
+count=$(git rev-list HEAD ^${latest} --ancestry-path ${latest} --count)
+export version=""
+ 
+case $branch in
+   "master")
+      version=${major}.$((minor+1)).0
+      ;;
+   "feature/*")
+      version=${major}.${minor}.${patch}-${branch}-${count}
+      ;;
+   *)
+      version=${major}.${minor}.$((patch+1))
+      ;;
+esac
+ 
+echo ${version} >> $BASH_ENV
+exit 0
