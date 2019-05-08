b02cd83e455f676687253f978c5d20957a01f2e9
Sarah Thompson <sethompson@upei.ca>
2019-05-08 14:23:56 -0300

    removed unnecessary margin

--- components/no_results_buttons.js
+++ components/no_results_buttons.js
@@ -2,26 +2,16 @@ import PropTypes from "prop-types";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import Button from "./button";
-import { globalTheme } from "../theme";
 
 const buttonBar = css`
   margin-top: 40px;
   text-align: center;
 `;
-const button = css`
-  @media only screen and (max-width: ${globalTheme.max.sm}) {
-    margin: 20px;
-  }
-`;
 
 const NoResultsButtons = props => {
   return (
     <div css={buttonBar}>
-      <Button
-        css={button}
-        id="reset_filters_button"
-        onClick={() => props.clearFilters()}
-      >
+      <Button id="reset_filters_button" onClick={() => props.clearFilters()}>
         {props.t("BenefitsPane.reset_filters")}
       </Button>
     </div>
