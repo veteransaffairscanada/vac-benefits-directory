14f12c9f5314c2203f59dfeca3c020f5ef8c70d6
Sarah Thompson <sethompson@upei.ca>
2019-08-15 13:28:01 -0300

    added disabled cookies files back

--- components/BB.js
+++ components/BB.js
@@ -9,10 +9,8 @@ import { css, jsx } from "@emotion/core";
 import Container from "../components/container";
 import { globalTheme } from "../theme";
 // import { DisabledCookiesBanner } from "./disabled_cookies_banner";
-import { areCookiesDisabled } from "../utils/common";
 import BenefitsPane from "./benefits_pane";
 import BreadCrumbs from "../components/breadcrumbs";
-import Cookies from "universal-cookie";
 import Paper from "./paper";
 import Header from "./typography/header";
 import ShareBox from "../components/share_box";
@@ -31,43 +29,6 @@ const topMatter = css`
 `;
 
 export class BB extends Component {
-  state = {
-    showDisabledCookieBanner: false
-  };
-
-  constructor(props) {
-    super(props);
-    this.cookies = new Cookies();
-  }
-
-  componentDidMount() {
-    this.props.setCookiesDisabled(areCookiesDisabled());
-    this.setState({
-      showDisabledCookieBanner: areCookiesDisabled()
-    });
-    // Update cookies if favourite benefits have been pruned on the server
-    let favouritesFromCookies = this.cookies.get("favouriteBenefits"),
-      favouriteBenefits = this.props.favouriteBenefits;
-
-    if (favouritesFromCookies && favouritesFromCookies.length > 0) {
-      const invalidBenefits = favouritesFromCookies.filter(
-        b => favouriteBenefits.indexOf(b) === -1
-      );
-      if (invalidBenefits.length > 0) {
-        this.cookies.set("favouriteBenefits", favouriteBenefits, { path: "/" });
-        this.props.saveFavourites(favouriteBenefits);
-      }
-    }
-  }
-
-  componentDidUpdate() {
-    if (this.state.showDisabledCookieBanner && !this.props.cookiesDisabled) {
-      this.setState({
-        showDisabledCookieBanner: false
-      });
-    }
-  }
-
   render() {
     const { t, url, store, guidedExperienceUrl, printUrl } = this.props; // eslint-disable-line no-unused-vars
 
@@ -112,18 +73,6 @@ export class BB extends Component {
               </div>
             </Grid>
             <Grid id="mainContent" item md={12} xs={12}>
-              <Grid container spacing={16}>
-                <Grid item xs={12}>
-                  {/* {this.state.showDisabledCookieBanner ? (
-                    <DisabledCookiesBanner
-                      t={t}
-                      onClose={() =>
-                        this.setState({ showDisabledCookieBanner: false })
-                      }
-                    />
-                  ) : null} */}
-                </Grid>
-              </Grid>
               <BenefitsPane id="BenefitsPane" t={t} store={store} url={url} />
             </Grid>
           </Grid>
@@ -133,25 +82,9 @@ export class BB extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setCookiesDisabled: areDisabled => {
-      dispatch({ type: "SET_COOKIES_DISABLED", data: areDisabled });
-    },
-    saveFavourites: favouriteBenefits => {
-      dispatch({
-        type: "LOAD_DATA",
-        data: { favouriteBenefits: favouriteBenefits }
-      });
-    }
-  };
-};
-
 const mapStateToProps = (reduxState, props) => {
   return {
-    cookiesDisabled: reduxState.cookiesDisabled,
     benefits: reduxState.benefits,
-    favouriteBenefits: reduxState.favouriteBenefits,
     guidedExperienceUrl: getGuidedExperienceUrl(reduxState, props),
     printUrl: getPrintUrl(reduxState, props, {})
   };
@@ -159,20 +92,11 @@ const mapStateToProps = (reduxState, props) => {
 
 BB.propTypes = {
   url: PropTypes.object.isRequired,
-  cookiesDisabled: PropTypes.bool.isRequired,
-  setCookiesDisabled: PropTypes.func.isRequired,
-  saveFavourites: PropTypes.func.isRequired,
   id: PropTypes.string.isRequired,
   guidedExperienceUrl: PropTypes.string,
   printUrl: PropTypes.string.isRequired,
   t: PropTypes.func.isRequired,
-  favouriteBenefits: PropTypes.array.isRequired,
   store: PropTypes.object
 };
 
-export default withTheme()(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(BB)
-);
+export default withTheme()(connect(mapStateToProps)(BB));
